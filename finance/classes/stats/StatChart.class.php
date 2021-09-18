3d1153729ca5eb445b2d4e805b3d90dad6a5c87a
Cédric Françoys <cedricfrancoys@gmail.com>
2021-09-18 11:15:51 +0200

    updates

--- finance/classes/stats/StatChart.class.php
+++ finance/classes/stats/StatChart.class.php
@@ -4,10 +4,10 @@
     Some Rights Reserved, Yesbabylon SRL, 2020-2021
     Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
 */
-namespace finance\accounting;
+namespace finance\stats;
 use equal\orm\Model;
 
-class AccountChart extends Model {
+class StatChart extends Model {
     
     public static function getName() {
         return "Chart of Accounts";
