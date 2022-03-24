cb574c0fd2a728451f9de0ce570a23c6b509d0ad
Cédric Françoys <cedricfrancoys@gmail.com>
2022-03-24 22:06:07 +0100

    added new files

---
+++ identity/classes/IdentityType.class.php
@@ -0,0 +1,34 @@
+<?php
+/*
+    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
+    Some Rights Reserved, Yesbabylon SRL, 2020-2021
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+namespace identity;
+use equal\orm\Model;
+
+class IdentityType extends Model {
+
+    public static function getColumns() {
+        return [
+            'name' => [
+                'type'              => 'alias',
+                'alias'             => 'description'
+            ],
+
+            'code' => [
+                'type'              => 'string',
+                'description'       => "Mnemonic of the identity type.",
+                'required'          => true
+            ],
+
+            'description' => [
+                'type'              => 'string',
+                'description'       => "Short description of the identity type.",
+                "multilang"         => true
+            ]
+            
+        ];
+    }
+
+}
