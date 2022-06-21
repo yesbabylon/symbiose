9739dd0829ee590737e4cacdd0bfdb33477297a5
Crosimo <jean.deborsu@hotmail.com>
2022-06-21 15:35:34 +0200

    added tree

--- sale/data/pos/session/tree.php
+++ sale/data/pos/session/tree.php
@@ -4,11 +4,13 @@
     Some Rights Reserved, Yesbabylon SRL, 2020-2021
     Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
 */
+
+use lodging\sale\pos\CashdeskSession;
 use sale\pos\Order;
 
 // announce script and fetch parameters values
 list($params, $providers) = announce([
-    'description'	=>	"Provide a fully loaded tree for a given order.",
+    'description'	=>	"Provide a fully loaded tree for a given session.",
     'params' 		=>	[
         'id' => [
             'description'   => 'Identifier of the order for which the tree is requested.',
@@ -19,8 +21,7 @@ list($params, $providers) = announce([
             'description'   => 'Type of tree being requested.',
             'type'          => 'string',
             'selection'     => [
-                'lines',        // tree with order lines
-                'payments'      // tree with order payments
+                'session'        
             ],
             'default'       => 'lines'
         ]
@@ -43,50 +44,25 @@ list($context) = [$providers['context']];
 $tree = [];
 
 switch($params['variant']) {
-    case 'lines':
+    case 'session':
         $tree = [
             'id',
-            'name',
-            'created',
+            'amount',
+            'user_id',
+            'cashdesk_id',
             'status',
-            'has_invoice',
-            'has_funding',
-            'total',
-            'price',
-            'order_lines_ids' => [
+            'orders_ids' => [
                 'id',
-                'order_id',
                 'name',
-                'unit_price',
-                'vat_rate',
-                'qty',
-                'discount',
-                'free_qty',
+                'created',
+                'status',
+                'has_invoice',
+                'has_funding',
                 'total',
-                'price'
-            ]
-        ];
-        break;
-    case 'payments':
-        $tree = [
-            'id',
-            'name',
-            'created',
-            'status',
-            'has_invoice',
-            'has_funding',
-            'total',
-            'price',
-            'customer_id',
-            'order_payments_ids' => [
-                'id',
-                'order_id',
-                'total_due',
-                'total_paid',
+                'price',
                 'order_lines_ids' => [
                     'id',
                     'order_id',
-                    'order_payment_id',
                     'name',
                     'unit_price',
                     'vat_rate',
@@ -96,28 +72,47 @@ switch($params['variant']) {
                     'total',
                     'price'
                 ],
-                'order_payment_parts_ids' => [
+                'order_payments_ids' => [
                     'id',
-                    'order_payment_id',
-                    'amount',
-                    'payment_method',
-                    'booking_id',
-                    'voucher_ref'
+                    'order_id',
+                    'total_due',
+                    'total_paid',
+                    'order_lines_ids' => [
+                        'id',
+                        'order_id',
+                        'order_payment_id',
+                        'name',
+                        'unit_price',
+                        'vat_rate',
+                        'qty',
+                        'discount',
+                        'free_qty',
+                        'total',
+                        'price'
+                    ],
+                    'order_payment_parts_ids' => [
+                        'id',
+                        'order_payment_id',
+                        'amount',
+                        'payment_method',
+                        'booking_id',
+                        'voucher_ref'
+                    ]
                 ]
-            ]
+            ]    
         ];
         break;
 }
 
-$orders = Order::id($params['id'])->read($tree)->adapt('txt')->get(true);
+$cashdesksessions = CashdeskSession::id($params['id'])->read($tree)->adapt('txt')->get(true);
 
-if(!$orders || !count($orders)) {
+if(!$cashdesksessions || !count($cashdesksessions)) {
     throw new Exception("unknown_order", QN_ERROR_UNKNOWN_OBJECT);
 }
 
-$order = reset($orders);
+$cashdesksession = reset($cashdesksessions);
 
 
 $context->httpResponse()
-        ->body($order)
+        ->body($cashdesksession)
         ->send();
