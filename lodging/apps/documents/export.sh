fd25c517db099eb2ac805b640a77f39d60f8558f
Cédric Françoys <cedricfrancoys@gmail.com>
2022-12-03 15:44:10 +0100

    removed lodging

--- lodging/apps/documents/export.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -rf ../../../../public/documents && mkdir ../../../../public/documents && cp -a dist/symbiose/* ../../../../public/documents/
--- lodging/views/sale/booking/Booking.list.default.md
+++
@@ -1,3 +0,0 @@
-# Booking List Default
-
-This is the default view for bookings listing.
