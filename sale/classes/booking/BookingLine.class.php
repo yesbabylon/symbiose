5001c6a82c49689de660f86a37523e594cd407c0
Cédric Françoys <cedricfrancoys@gmail.com>
2022-12-12 15:14:55 +0100

    improved computed fields reset conditions

--- sale/classes/booking/BookingLine.class.php
+++ sale/classes/booking/BookingLine.class.php
@@ -295,7 +295,7 @@ class BookingLine extends Model {
     public static function _resetPrices($om, $oids, $values, $lang) {
         trigger_error("QN_DEBUG_ORM::calling sale\booking\BookingLine:_resetPrices", QN_REPORT_DEBUG);
 
-        $lines = $om->read(self::getType(), $oids, ['has_manual_unit_price', 'has_manual_vat_rate', 'booking_line_group_id'], $lang);
+        $lines = $om->read(self::getType(), $oids, ['price_id', 'has_manual_unit_price', 'has_manual_vat_rate', 'booking_line_group_id'], $lang);
 
         if($lines > 0) {
             $new_values = ['vat_rate' => null, 'unit_price' => null, 'total' => null, 'price' => null, 'fare_benefit' => null, 'discount' => null, 'free_qty' => null];
@@ -312,12 +312,12 @@ class BookingLine extends Model {
             // update lines
             foreach($lines as $lid => $line) {
                 $assigned_values = $new_values;
-                // don't reset unit_price for products that have a price with a value of 0.0
-                if($line['has_manual_unit_price']) {
+                // don't reset unit_price for products that have a manual unit price set or that are not linked to a Price object
+                if($line['has_manual_unit_price'] || !$line['price_id']) {
                     unset($assigned_values['unit_price']);
                 }
-                // don't reset vat_rate for products that have a rate with a value of 0.0
-                if($line['has_manual_vat_rate']) {
+                // don't reset vat_rate for products that have a manual vat rate set or that are not linked to a Price object
+                if($line['has_manual_vat_rate'] || !$line['price_id']) {
                     unset($assigned_values['vat_rate']);
                 }
                 $om->update(self::getType(), $lid, $assigned_values);
