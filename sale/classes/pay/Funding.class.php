bafa8f25e002df88930919750fcc8b9303f9f210
Cédric Françoys <cedricfrancoys@gmail.com>
2023-01-09 10:53:31 +0100

    updates

--- sale/classes/pay/Funding.class.php
+++ sale/classes/pay/Funding.class.php
@@ -186,11 +186,11 @@ class Funding extends Model {
      */
     public static function canupdate($om, $oids, $values, $lang) {
         if(count($values) > 1 || !isset($values['is_paid'])) {
-            $fundings = $om->read(self::getType(), $oids, ['is_paid', 'due_amount', 'paid_amount'], $lang);
+            $fundings = $om->read(self::getType(), $oids, ['is_paid', 'due_amount', 'paid_amount', 'payments_ids'], $lang);
             if($fundings > 0) {
                 foreach($fundings as $funding) {
-                    if($funding['is_paid'] && $funding['due_amount'] == $funding['paid_amount']) {
-                        return ['is_paid' => ['non_editable' => 'No change is allowed once the funding has been paid.']];
+                    if($funding['is_paid'] && $funding['due_amount'] == $funding['paid_amount'] && count($funding['payments_ids'])) {
+                        return ['is_paid' => ['non_editable' => 'No change is allowed once the funding has been fully paid.']];
                     }
                 }
             }
