c73bf0e4c7c680c4ee9009c57c8b7c19803bc126
Cédric Françoys <cedricfrancoys@gmail.com>
2023-02-21 15:42:14 +0100

    cleaned up packages

--- classes-creator/apps/create/export.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -rf ../../../../public/modeleditor && mkdir ../../../../public/modeleditor && cp -a dist/symbiose/* ../../../../public/modeleditor/
--- yesbabylon/apps/yesbabylon/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf .angular
-npm link sb-shared-lib
-ng build --configuration production --output-hashing none --base-href="//documents\\"
