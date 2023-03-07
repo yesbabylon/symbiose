ba3d2575531e5e968901b07308f6428f66d9bc8d
Cédric Françoys <cedricfrancoys@gmail.com>
2023-03-07 16:20:53 +0100

    updates

---
+++ core/data/config/constant.php
@@ -0,0 +1,42 @@
+<?php
+/*
+    This file is part of the eQual framework <http://www.github.com/cedricfrancoys/equal>
+    Some Rights Reserved, Cedric Francoys, 2010-2021
+    Licensed under GNU LGPL 3 license <http://www.gnu.org/licenses/>
+*/
+list($params, $providers) = announce([
+    'description'   => 'Request the updated list of absences from SDworx since last query (from setting sd_sync.last_run), and store the received data in the Absences objects.',
+    'params'        => [
+        'constant' =>  [
+            'type'          => 'string',
+            'description'   => 'name of the constant for which the value is requested.',
+            'required'      => true
+        ]
+    ],
+    'response'      => [
+        'content-type'  => 'application/json',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'access' => [
+        'visibility'    => 'private'
+    ],
+    'providers'     => [ 'context', 'report' ]
+]);
+
+/**
+ * @var \equal\php\Context                $context
+ * @var \equal\error\Reporter             $reporter
+ */
+list($context, $reporter) = [ $providers['context'], $providers['report'] ];
+
+if(!\config\constant($params['constant']) && !defined($params['constant'])) {
+    throw new Exception('unknown_property', QN_ERROR_INVALID_PARAM);
+}
+
+\config\export($params['constant']);
+
+$context->httpResponse()
+        ->status(200)
+        ->body(['result' => constant($params['constant'])])
+        ->send();
