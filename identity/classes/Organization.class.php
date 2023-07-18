ea9a003ceacfc694e7da54c2450e562a30943a07
Cédric Françoys <cedricfrancoys@gmail.com>
2023-07-18 14:36:26 +0200

    added

---
+++ identity/classes/Organization.class.php
@@ -0,0 +1,41 @@
+<?php
+/*
+    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
+    Some Rights Reserved, Yesbabylon SRL, 2020-2021
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+namespace identity;
+
+class Organization extends Identity {
+
+    public static function getName() {
+        return "Organization";
+    }
+
+    public function getTable() {
+        // force table name to use distinct tables and ID columns
+        return 'identity_organization';
+    }
+
+    public static function getDescription() {
+        return "Organizations are the legal entities to which the ERP is dedicated. By convention, the main Organization has the ID 1.";
+    }
+
+    public static function getColumns() {
+        return [
+            'type_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\IdentityType',
+                'onupdate'          => 'onupdateTypeId',
+                'default'           => 3,
+                'description'       => 'Type of identity.'
+            ],
+
+            'type' => [
+                'type'              => 'string',
+                'default'           => 'C',
+                'readonly'          => true
+            ]
+        ];
+    }
+}
