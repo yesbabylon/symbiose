b7b4311a100d1f599b38fe4ae2ceb1b0a7903138
Cédric Françoys <cedricfrancoys@gmail.com>
2023-07-18 14:36:54 +0200

    removed non-standard properties

--- sale/classes/customer/Customer.class.php
+++ sale/classes/customer/Customer.class.php
@@ -24,11 +24,11 @@ class Customer extends \identity\Partner {
 
         return [
 
-            // if partner is a customer, it can be assigned to a rate class
             'rate_class_id' => [
                 'type'              => 'many2one',
                 'foreign_object'    => 'sale\customer\RateClass',
-                'description'       => 'Rate class that applies to the customer.',
+                'description'       => "Rate class that applies to the customer.",
+                'help'              => "If partner is a customer, it can be assigned to a rate class.",
                 'visible'           => ['relationship', '=', 'customer'],
                 'default'           => 1,
                 'readonly'          => true
@@ -38,16 +38,16 @@ class Customer extends \identity\Partner {
                 'type'              => 'many2one',
                 'foreign_object'    => 'sale\customer\CustomerNature',
                 'description'       => 'Nature of the customer (map with rate classes).',
-                'onupdate'          => 'sale\customer\Customer::onupdateCustomerNatureId'
+                'onupdate'          => 'onupdateCustomerNatureId'
             ],
 
-            // if partner is a customer, it can be assigned a customer type
             'customer_type_id' => [
                 'type'              => 'many2one',
                 'foreign_object'    => 'sale\customer\CustomerType',
-                'description'       => 'Type of customer (map with rate classes).',
+                'description'       => "Type of customer (map with rate classes). Defaults to 'individual'.",
+                'help'              => "If partner is a customer, it can be assigned a customer type",
                 'visible'           => ['relationship', '=', 'customer'],
-                'default'           => 1                                                // default is 'individual'
+                'default'           => 1
             ],
 
             'relationship' => [
@@ -56,39 +56,11 @@ class Customer extends \identity\Partner {
                 'description'       => 'Force relationship to Customer'
             ],
 
-            'is_tour_operator' => [
-                'type'              => 'boolean',
-                'description'       => 'Mark the customer as a Tour Operator.',
-                'default'           => false
-            ],
-
-            'count_booking_12' => [
-                'type'              => 'computed',
-                'result_type'       => 'integer',
-                'function'          => 'sale\customer\Customer::calcCountBooking12',
-                'description'       => 'Number of bookings made during last 12 months (one year).'
-            ],
-
-            'count_booking_24' => [
-                'type'              => 'computed',
-                'result_type'       => 'integer',
-                'function'          => 'sale\customer\Customer::calcCountBooking24',
-                'description'       => 'Number of bookings made during last 24 months (2 years).'
-            ],
-
             'address' => [
                 'type'              => 'computed',
                 'result_type'       => 'string',
                 'function'          => 'calcAddress',
-                'description'       => 'Main address from related Identity.',
-                'store'             => true
-            ],
-
-            'bookings_ids' => [
-                'type'              => 'one2many',
-                'foreign_object'    => 'sale\booking\Booking',
-                'foreign_field'     => 'customer_id',
-                'description'       => "The bookings history of the customer.",
+                'description'       => 'Main address from related Identity.'
             ],
 
             'ref_account' => [
@@ -101,15 +73,14 @@ class Customer extends \identity\Partner {
         ];
     }
 
-    public static function onupdateCustomerNatureId($om, $oids, $values, $lang) {
-        $customers = $om->read(__CLASS__, $oids, ['customer_nature_id.rate_class_id', 'customer_nature_id.customer_type_id']);
-        if($customers > 0 && count($customers)) {
-            foreach($customers as $cid => $customer) {
-                $customer_type_id = $customer['customer_nature_id.customer_type_id'];
-                $rate_class_id = $customer['customer_nature_id.rate_class_id'];
-                if(!empty($customer_type_id) && !empty($rate_class_id)) {
-                    $om->write(__CLASS__, $oids, ['rate_class_id' => $rate_class_id, 'customer_type_id' => $customer_type_id]);
-                }
+    public static function onupdateCustomerNatureId($self) {
+        $self->read(['customer_nature_id' => ['rate_class_id', 'customer_type_id']]);
+        foreach($self as $id => $customer) {
+            if($customer['customer_nature_id']) {
+                self::id($id)->update([
+                        'rate_class_id' => $customer['customer_nature_id']['rate_class_id'],
+                        'customer_type_id' => $customer['customer_nature_id']['customer_type_id']
+                    ]);
             }
         }
     }
@@ -118,53 +89,13 @@ class Customer extends \identity\Partner {
      * Computes the number of bookings made by the customer during the last two years.
      *
      */
-    public static function calcAddress($om, $oids, $lang) {
+    public static function calcAddress($self) {
         $result = [];
-
-        $customers = $om->read(__CLASS__, $oids, ['partner_identity_id.address_street', 'partner_identity_id.address_city'], $lang);
-        foreach($customers as $oid => $customer) {
-            $result[$oid] = "{$customer['partner_identity_id.address_street']} {$customer['partner_identity_id.address_city']}";
+        $self->read(['partner_identity_id' => ['address_street', 'address_city']]);
+        foreach($self as $id => $customer) {
+            $result[$id] = "{$customer['partner_identity_id']['address_street']} {$customer['partner_identity_id']['address_city']}";
         }
         return $result;
     }
 
-    /**
-     * Computes the number of bookings made by the customer during the last 12 months.
-     *
-     */
-    public static function calcCountBooking12($om, $oids, $lang) {
-        $result = [];
-        $time = time();
-        $from = mktime(0, 0, 0, date('m', $time)-12, date('d', $time), date('Y', $time));
-        foreach($oids as $oid) {
-            $bookings_ids = $om->search('sale\booking\Booking', [
-                ['customer_id', '=', $oid],
-                ['created', '>=', $from],
-                ['is_cancelled', '=', false],
-                ['status', 'not in', ['quote', 'option']]
-            ]);
-            $result[$oid] = count($bookings_ids);
-        }
-        return $result;
-    }
-
-    /**
-     * Computes the number of bookings made by the customer during the last two years.
-     *
-     */
-    public static function calcCountBooking24($om, $oids, $lang) {
-        $result = [];
-        $time = time();
-        $from = mktime(0, 0, 0, date('m', $time)-24, date('d', $time), date('Y', $time));
-        foreach($oids as $oid) {
-            $bookings_ids = $om->search('sale\booking\Booking', [
-                ['customer_id', '=', $oid],
-                ['created', '>=', $from],
-                ['is_cancelled', '=', false],
-                ['status', 'not in', ['quote', 'option']]
-            ]);
-            $result[$oid] = count($bookings_ids);
-        }
-        return $result;
-    }
 }
