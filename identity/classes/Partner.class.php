3c094d4aa0c8e53cb0077fc2d60a0fbdb514f53c
Cédric Françoys <cedricfrancoys@gmail.com>
2023-07-18 14:35:38 +0200

    made computed field not storable

--- identity/classes/Partner.class.php
+++ identity/classes/Partner.class.php
@@ -14,7 +14,7 @@ class Partner extends Model {
     }
 
     public static function getDescription() {
-        return "A Partner describes a relationship between two Identities (contact, employee, customer, provider, payer, other).";
+        return "A Partner entity describes a relationship between two Identities (contact, employee, customer, provider, payer, other).";
     }
 
     public static function getColumns() {
@@ -28,18 +28,30 @@ class Partner extends Model {
                 'description'       => 'The display name of the partner (related organisation name).'
             ],
 
+            'organisation_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\Organization',
+                'description'       => 'Organisation of the current installation the partner relates to.',
+                'default'           => 1
+            ],
+
+            'is_internal' => [
+                'type'              => 'boolean',
+                'description'       => 'The partnership relates to (one of) the organization(s) from the current installation.',
+                'default'           => true
+            ],
+
             'owner_identity_id' => [
                 'type'              => 'many2one',
-                'foreign_object'    => Identity::getType(),
+                'foreign_object'    => 'identity\Identity',
                 'description'       => 'The identity organisation which the targeted identity is a partner of.',
-                'default'           => 1
+                'visibility'        => ['is_internal', '=', false]
             ],
 
             'partner_identity_id' => [
                 'type'              => 'many2one',
-                'foreign_object'    => Identity::getType(),
+                'foreign_object'    => 'identity\Identity',
                 'description'       => 'The targeted identity (the partner).',
-                'onupdate'          => 'onupdatePartnerIdentityId',
                 'required'          => true
             ],
 
@@ -102,22 +114,22 @@ class Partner extends Model {
                 'function'          => 'calcMobile',
                 'result_type'       => 'string',
                 'usage'             => 'phone',
-                'description'       => 'Mobile phone number of the contact (from Identity).'
+                'description'       => 'Mobile phone number of the contact (from Identity).',
             ],
 
             'title' => [
                 'type'              => 'computed',
                 'function'          => 'calcTitle',
                 'result_type'       => 'string',
-                'description'       => 'Title of the contact (from Identity).'
-                // #memo - title origin remains the related identity
+                'description'       => 'Title of the contact (from Identity).',
             ],
 
             'lang_id' => [
-                'type'              => 'many2one',
+                'type'              => 'computed',
+                'result_type'       => 'many2one',
                 'foreign_object'    => 'core\Lang',
                 'description'       => "Preferred language of the partner (relates to identity).",
-                'default'           => 1
+                'function'          => 'calcLangId'
             ],
 
             'is_active' => [
@@ -134,18 +146,6 @@ class Partner extends Model {
         ];
     }
 
-    public static function onupdatePartnerIdentityId($om, $oids, $values, $lang) {
-        $res = $om->read(get_called_class(), $oids, [ 'partner_identity_id.lang_id' ], $lang);
-        if($res > 0 && count($res) ) {
-            foreach($res as $oid => $odata) {
-                $om->write(get_called_class(), $oids, [ 'lang_id' => $odata['partner_identity_id.lang_id'] ], $lang);
-            }
-        }
-        $om->write(get_called_class(), $oids, [ 'name' => null, 'title' => null, 'phone' => null, 'email' => null ], $lang);
-        // force immediate re-computing of the name
-        $om->read(get_called_class(), $oids, [ 'name' ], $lang);
-    }
-
     public static function calcName($om, $oids, $lang) {
         $result = [];
         $partners = $om->read(self::getType(), $oids, ['partner_identity_id.name'], $lang);
@@ -157,50 +157,47 @@ class Partner extends Model {
         return $result;
     }
 
-    public static function calcEmail($om, $oids, $lang) {
+    public static function calcEmail($self) {
         $result = [];
-        $partners = $om->read(get_called_class(), $oids, ['partner_identity_id.email'], $lang);
-        foreach($partners as $oid => $partner) {
-            $result[$oid] = '';
-            if(isset($partner['partner_identity_id.email'])) {
-                $result[$oid] = $partner['partner_identity_id.email'];
-            }
+        $self->read(['partner_identity_id' => ['email']]);
+        foreach($self as $oid => $partner) {
+            $result[$oid] = $partner['partner_identity_id']['email'];
         }
         return $result;
     }
 
-    public static function calcPhone($om, $oids, $lang) {
+    public static function calcPhone($self) {
         $result = [];
-        $partners = $om->read(get_called_class(), $oids, ['partner_identity_id.phone'], $lang);
-        foreach($partners as $oid => $partner) {
-            $result[$oid] = '';
-            if(isset($partner['partner_identity_id.phone'])) {
-                $result[$oid] = $partner['partner_identity_id.phone'];
-            }
+        $self->read(['partner_identity_id' => ['phone'] ]);
+        foreach($self as $oid => $partner) {
+            $result[$oid] = $partner['partner_identity_id.phone'];
         }
         return $result;
     }
 
-    public static function calcMobile($om, $oids, $lang) {
+    public static function calcMobile($self) {
         $result = [];
-        $partners = $om->read(get_called_class(), $oids, ['partner_identity_id.mobile'], $lang);
-        foreach($partners as $oid => $partner) {
-            $result[$oid] = '';
-            if(isset($partner['partner_identity_id.mobile'])) {
-                $result[$oid] = $partner['partner_identity_id.mobile'];
-            }
+        $self->read(['partner_identity_id' => ['mobile']]);
+        foreach($self as $id => $partner) {
+            $result[$id] = $partner['partner_identity_id']['mobile'];
         }
         return $result;
     }
 
-    public static function calcTitle($om, $oids, $lang) {
+    public static function calcTitle($self) {
         $result = [];
-        $partners = $om->read(get_called_class(), $oids, ['partner_identity_id.title'], $lang);
-        foreach($partners as $oid => $partner) {
-            $result[$oid] = '';
-            if(isset($partner['partner_identity_id.title'])) {
-                $result[$oid] = $partner['partner_identity_id.title'];
-            }
+        $self->read(['partner_identity_id']['title']);
+        foreach($self as $id => $partner) {
+            $result[$id] = $partner['partner_identity_id']['title'];
+        }
+        return $result;
+    }
+
+    public static function calcLangId($self) {
+        $result = [];
+        $self->read(['partner_identity_id' => ['lang_id']]);
+        foreach($self as $id => $partner) {
+            $result[$id] = $partner['partner_identity_id']['lang_id'];
         }
         return $result;
     }
@@ -208,23 +205,16 @@ class Partner extends Model {
     /**
      * Signature for single object change from views.
      *
-     * @param  Object   $om        Object Manager instance.
      * @param  Array    $event     Associative array holding changed fields as keys, and their related new values.
      * @param  Array    $values    Copy of the current (partial) state of the object (fields depend on the view).
-     * @param  String   $lang      Language (char 2) in which multilang field are to be processed.
      * @return Array    Associative array mapping fields with their resulting values.
      */
-    public static function onchange($om, $event, $values, $lang='en') {
+    public static function onchange($event, $values) {
         $result = [];
-
         if(isset($event['partner_identity_id'])) {
-            $identities = $om->read('identity\Identity', $event['partner_identity_id'], ['name']);
-            if($identities > 0) {
-                $identity = reset($identities);
-                $result['name'] = $identity['name'];
-            }
+            $identity = Identity::id($event['partner_identity_id'])->read(['name'])->first();
+            $result['name'] = $identity['name'];
         }
-
         return $result;
     }
 }
