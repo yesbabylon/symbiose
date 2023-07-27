5f71c8f806569d1536a7c0d6af0066eb77866bf5
Cédric Françoys <cedricfrancoys@gmail.com>
2023-07-27 17:15:42 +0200

    removed reference to lodging

--- sale/classes/price/PriceList.class.php
+++ sale/classes/price/PriceList.class.php
@@ -150,15 +150,6 @@ class PriceList extends Model {
             $cron = $providers['cron'];
 
             foreach($pricelists as $pid => $pricelist) {
-                if($pricelist['status'] == 'published') {
-                    // add a task to the CRON for updating status of bookings waiting for the pricelist
-                    $cron->schedule(
-                        "booking.is_tbc.confirm",
-                        time() + 60,
-                        'lodging_pricelist_check-bookings',
-                        [ 'id' => $pid ]
-                    );
-                }
                 // immediate re-compute prices is_active flag
                 $om->update('sale\price\Price', $pricelist['prices_ids'], ['is_active' => null]);
                 $om->read('sale\price\Price', $pricelist['prices_ids'], ['is_active']);
