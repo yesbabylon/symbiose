8cf858b0f28c33b70bdeebdde03e04e818fbce97
Lucas Laurent <lucas@yesbabylon.com>
2024-01-03 12:49:24 +0100

    Add new action to shift the period of a subscription depending on duration + enhance actions organization

---
+++ inventory/actions/service/subscription/shift-period.php
@@ -0,0 +1,45 @@
+<?php
+
+use inventory\service\Subscription;
+
+list($params, $providers) = eQual::announce([
+    'description' => 'Shift subscription dates depending on duration.',
+    'params'      => [
+        'id' =>  [
+            'description' => 'ID of the subscription.',
+            'type'        => 'integer',
+            'required'    => true
+        ]
+    ],
+    'response'    => [
+        'content-type'  => 'application/json',
+        'charset'       => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'providers'   => ['context', 'orm']
+]);
+
+list($context, $orm) = [$providers['context'], $providers['orm']];
+
+$subscription = Subscription::id($params['id'])
+    ->read([
+        'id',
+        'date_from',
+        'date_to',
+        'duration'
+    ])
+    ->first();
+
+if(!$subscription) {
+    throw new Exception('unknown_subscription', QN_ERROR_UNKNOWN_OBJECT);
+}
+
+Subscription::id($subscription['id'])
+    ->update([
+        'date_from' => $subscription['date_to'],
+        'date_to'   => strtotime(Subscription::MAP_DURATION[$subscription['duration']], $subscription['date_to'])
+    ]);
+
+$context->httpResponse()
+        ->status(204)
+        ->send();
