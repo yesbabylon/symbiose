2defe16048498a6a745e9d2cfe54735abef3cbda
Cédric Françoys <cedricfrancoys@gmail.com>
2024-01-05 11:52:57 +0100

    fixed excessive_amount condition

--- sale/classes/pay/Payment.class.php
+++ sale/classes/pay/Payment.class.php
@@ -177,8 +177,12 @@ class Payment extends Model {
                 return ['is_exported' => ['non_editable' => 'Once exported a payment can no longer be updated.']];
             }
             if($payment['payment_origin'] == 'bank') {
-                if(isset($values['amount']) && round($values['amount'], 2) > round($payment['statement_line_id.remaining_amount'], 2)) {
-                    return ['amount' => ['excessive_amount' => "Sum of assigned payments ({$values['amount']}) cannot be higher than statement line amount ({$payment['statement_line_id.remaining_amount']})."]];
+                if(isset($values['amount'])) {
+                    $payment_amount = round($values['amount'], 2);
+                    $remaining_amount = round($payment['statement_line_id.remaining_amount'], 2) - $payment_amount;
+                    if($remaining_amount <= 0) {
+                        return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$payment['statement_line_id.remaining_amount']})."]];
+                    }
                 }
             }
         }
