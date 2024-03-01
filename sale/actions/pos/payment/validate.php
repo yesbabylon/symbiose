24032b2d6ce1ef297f295db1494eecb5b9e5bde1
Cédric Françoys <cedricfrancoys@gmail.com>
2024-03-01 15:28:34 +0100

    removed Operation creation at OrderPayment validation

--- sale/actions/pos/payment/validate.php
+++ sale/actions/pos/payment/validate.php
@@ -5,8 +5,6 @@
     Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
 */
 use sale\pos\OrderPayment;
-use sale\pos\Operation;
-
 
 list($params, $providers) = announce([
     'description'   => "Validate a partial payment of a cashdesk order.",
@@ -35,9 +33,7 @@ list($context, $orm, $cron) = [$providers['context'], $providers['orm'], $provid
 // read payment object
 $payment = OrderPayment::id($params['id'])
                         ->read([
-                            'id', 'creator', 'status', 'total_paid', 'total_due',
-                            'order_id'                  => ['session_id' => ['id', 'cashdesk_id']],
-                            'order_payment_parts_ids'   => ['amount', 'payment_method']
+                            'id', 'creator', 'status', 'total_paid', 'total_due'
                         ])
                         ->first();
 
@@ -53,48 +49,8 @@ if($payment['total_paid'] < $payment['total_due']) {
     throw new Exception("unbalanced_payment", QN_ERROR_NOT_ALLOWED);
 }
 
-$order = $payment['order_id'];
-$session = $order['session_id'];
-
 // set payment status to paid
 OrderPayment::id($params['id'])->update(['status' => 'paid']);
-
-// create cash-in operation
-$cash_in = 0.0;
-foreach($payment['order_payment_parts_ids'] as $pid => $part) {
-    if($part['payment_method'] == 'cash' && $part['amount'] > 0) {
-        $cash_in += $part['amount'];
-    }
-}
-
-if($cash_in > 0.0) {
-    Operation::create([
-        'amount'        => $cash_in,
-        'type'          => 'sale',
-        'user_id'       => $payment['creator'],
-        'session_id'    => $session['id']
-    ]);
-}
-
-// create cash-out operation, if any
-$cash_out = 0;
-if($payment['total_due'] < 0) {
-    // cash refunding
-    $cash_out = $payment['total_due'];
-}
-elseif($payment['total_paid'] > $payment['total_due']) {
-    // too much cash given
-    $cash_out = $payment['total_due'] - $payment['total_paid'];
-}
-
-if($cash_out < 0) {
-    Operation::create([
-        'amount'        => round($cash_out, 2),
-        'type'          => 'sale',
-        'user_id'       => $payment['creator'],
-        'session_id'    => $session['id']
-    ]);
-}
 
 $context->httpResponse()
         ->status(204)
