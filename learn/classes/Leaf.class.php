a0de58d94f0365d63e2eb988ac52002baa054174
Cédric Françoys <cedricfrancoys@gmail.com>
2024-04-02 18:38:37 +0200

    renamed qursus to learn

---
+++ learn/classes/Leaf.class.php
@@ -0,0 +1,119 @@
+<?php
+/*
+    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
+    Some Rights Reserved, Yesbabylon SRL, 2020-2021
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+namespace learn;
+
+use equal\orm\Model;
+
+class Leaf extends Model {
+
+    public static function getColumns() {
+        return [
+            'identifier' => [
+                'type'              => 'integer',
+                'description'       => 'Unique identifier of the leaf within the page.',
+                'onupdate'          => 'onupdateVisibility',
+                'default'           => 1
+            ],
+
+            'order' => [
+                'type'              => 'integer',
+                'description'       => 'Position of the leaf in the page.',
+                'default'           => 1
+            ],
+
+            'visible' => [
+                'type'              => 'computed',
+                'function'          => 'calcVisible',
+                'result_type'       => 'string',
+                'store'             => true,
+            ],
+
+            'visibility_rule' => [
+                'type'              => 'string',
+                'selection'         => [
+                    'always visible'                    => 'always visible',
+                    '$page.selection = $identifier'     => 'selection matches identifier',
+                    '$page.submitted = true'            => 'page submitted',
+                    '$page.submitted = false'           => 'page not submitted'
+                ],
+                'default'           => 'always visible',
+                'onupdate'          => 'onupdateVisibility'
+            ],
+
+            'groups' => [
+                'type'              => 'alias',
+                'alias'             => 'groups_ids'
+            ],
+
+            'groups_ids' => [
+                'type'              => 'one2many',
+                'foreign_object'    => 'learn\Group',
+                'foreign_field'     => 'leaf_id',
+                'order'             => 'order',
+                'sort'              => 'asc',
+                'ondetach'          => 'delete'
+            ],
+
+            'background_image' => [
+                'type'              => 'string',
+                'description'       => "URL of the background image."
+            ],
+
+            'background_stretch' => [
+                'type'              => 'boolean',
+                'description'       => "JSON formatted array of visibility domain for leaf.",
+                'default'           => false
+            ],
+
+            'background_opacity' => [
+                'type'              => 'float',
+                'description'       => "Opacity of the background (from 0 to 1).",
+                'default'           => 0.5
+            ],
+
+            'contrast' => [
+                'type'              => 'string',
+                'selection'         => ['dark', 'light'],
+                'default'           => 'light'
+            ],
+
+            'page_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'learn\Page',
+                'description'       => 'Page the leaf relates to.',
+                'ondelete'          => 'cascade'         // delete leaf when parent page is deleted
+            ]
+
+        ];
+    }
+
+    public static function calcVisible($om, $oids, $lang) {
+        $result = [];
+
+        $leaves = $om->read(__CLASS__, $oids, ['identifier', 'visibility_rule'], $lang);
+
+        foreach($leaves as $oid => $leaf) {
+            if($leaf['visibility_rule'] == 'always visible') {
+                $result[$oid] = "[]";
+            }
+            else {
+                $rule = str_replace('$identifier', $leaf['identifier'], $leaf['visibility_rule']);
+                list($operand, $operator, $value) = explode(' ', $rule);
+                if(!is_numeric($value) && !in_array($value, ['true', 'false'])) {
+                    $value = "'$value'";
+                }
+                $result[$oid] = "['$operand','$operator',$value]";
+            }
+        }
+
+        return $result;
+    }
+
+    public static function onupdateVisibility($om, $oids, $values, $lang) {
+        $om->write(__CLASS__, $oids, ['visible' => null], $lang);
+    }
+}
---
+++ learn/data/course.php
@@ -0,0 +1,115 @@
+<?php
+
+use learn\Course;
+use learn\UserAccess;
+
+
+list($params, $providers) = announce([
+    'description' => "Returns the given course with all of its nested entities.",
+    'deprecated' => true,
+    'params' => [
+        'course_id' => [
+            'description' => 'Course identifier (id field).',
+            'type' => 'integer',
+            'required' => true
+        ],
+    ],
+    'response' => [
+        'content-type' => 'application/json',
+        'charset' => 'utf-8',
+        'accept-origin' => '*'
+    ],
+    'providers' => ['context', 'orm', 'auth']
+]);
+
+list($context, $orm, $auth) = [$providers['context'], $providers['orm'], $providers['auth']];
+
+$user_id = $auth->userId();
+
+$access_ids = UserAccess::search([['course_id', '=', $params['course_id']], ['user_id', '=', $user_id]])->ids();
+
+if (!count($access_ids)) {
+    throw new Exception('missing_licence', QN_ERROR_NOT_ALLOWED);
+}
+
+$data = Course::search(['id', '=', $params['course_id']])->read([
+    'name',
+    'title',
+    'subtitle',
+    'description',
+    'langs_ids'=> ['id', 'name', 'code'],
+    'modules' => [
+        'id',
+        'identifier',
+        'order',
+        'title',
+        'description',
+        'duration',
+        'page_count',
+        'chapter_count',
+        'chapters' => [
+            'id',
+            'identifier',
+            'order',
+            'title',
+            'duration',
+            'description',
+            'page_count',
+//            'pages' => [
+//                'id',
+//                'identifier',
+//                'order',
+////                'next_active',
+////                'next_active_rule',
+//                // ! Cut here
+//                'leaves' => [
+//                    'id',
+//                    'identifier',
+//                    'order',
+////                    'visible',
+////                    'visibility_rule',
+//                    'background_image',
+//                    'background_stretch',
+//                    'background_opacity',
+//                    'contrast',
+//                    'groups' => [
+//                        'id',
+////                        'identifier',
+//                        'order',
+//                        'direction',
+//                        'row_span',
+////                        'visible',
+////                        'visibility_rule',
+//                        'fixed',
+//                        'widgets' => [
+//                            'id',
+//                            'identifier',
+//                            'order',
+//                            'content',
+////                            'type',
+//                            'image_url',
+//                            'video_url',
+//                            'sound_url',
+//                            'has_separator_left',
+//                            'has_separator_right',
+//                            'align',
+//                            'on_click'
+//                        ]
+//                    ]
+//                ],
+//            ]
+        ]
+    ]
+],
+    $params['lang']
+)
+    ->adapt('json')
+    ->first(true);
+
+if (!$data) {
+    throw new Exception("unknown_entity", QN_ERROR_INVALID_PARAM);
+}
+
+$context->httpResponse()
+    ->body($data)
+    ->send();
