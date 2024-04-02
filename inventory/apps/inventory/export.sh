35c01cc89f5b64e7b2a7c8e58682ec1e38698437
Cédric Françoys <cedricfrancoys@gmail.com>
2024-04-02 18:50:38 +0200

    removed inventory app (std-app instead)

--- inventory/apps/inventory/export.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# cp -r version ../version && cp -r web.app ../web.app && cp -r manifest.json ../manifest.json
-rm -rf ../../../../public/inventory && mkdir ../../../../public/inventory && cp -a dist/symbiose/* ../../../../public/inventory/
