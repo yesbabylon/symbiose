a5207d19641b6c4006c953f057bddc976fe35fa2
Cédric Françoys <cedricfrancoys@gmail.com>
2024-05-09 16:55:49 +0200

    refactored calc and onupdate callbacks

--- sale/classes/price/Price.class.php
+++ sale/classes/price/Price.class.php
@@ -6,6 +6,7 @@
 */
 namespace sale\price;
 use equal\orm\Model;
+use finance\accounting\AccountingRule;
 
 class Price extends Model {
     public static function getColumns() {
@@ -23,21 +24,22 @@ class Price extends Model {
                 'type'              => 'float',
                 'usage'             => 'amount/money:4',
                 'description'       => "Tax excluded price.",
-                'onupdate'          => 'onupdatePrice',
-                'required'          => true
+                'required'          => true,
+                'dependents'        => ['price_vat'],
+                'visible'           => ['price_type', '=', 'direct']
             ],
 
             'price_vat' => [
                 'type'              => 'computed',
                 'result_type'       => 'float',
                 'function'          => 'calcPriceVat',
-                'usage'             => 'amount/money:4',
+                'usage'             => 'amount/money:2',
                 'description'       => "Tax included price. This field is used to allow encoding prices VAT incl.",
                 'store'             => true,
-                'onupdate'          => 'onupdatePriceVat'
+                'visible'           => ['price_type', '=', 'direct']
             ],
 
-            'type' => [
+            'price_type' => [
                 'type'              => 'string',
                 'selection'         => ['direct', 'computed'],
                 'default'           => 'direct'
@@ -55,7 +57,7 @@ class Price extends Model {
                 'description'       => "The Price List the price belongs to.",
                 'required'          => true,
                 'ondelete'          => 'cascade',
-                'onupdate'          => 'onupdatePriceListId'
+                'dependents'        => ['name']
             ],
 
             'is_active' => [
@@ -68,9 +70,9 @@ class Price extends Model {
 
             'accounting_rule_id' => [
                 'type'              => 'many2one',
-                'foreign_object'    => \finance\accounting\AccountingRule::getType(),
+                'foreign_object'    => 'finance\accounting\AccountingRule',
                 'description'       => "Selling accounting rule. If set, overrides the rule of the product this price is assigned to.",
-                'onupdate'          => 'onupdateAccountingRuleId'
+                'dependents'        => ['vat_rate', 'price_vat']
             ],
 
             'product_id' => [
@@ -78,7 +80,7 @@ class Price extends Model {
                 'foreign_object'    => 'sale\catalog\Product',
                 'description'       => "The Product (sku) the price applies to.",
                 'required'          => true,
-                'onupdate'          => 'onupdateProductId'
+                'dependents'        => ['name']
             ],
 
             'vat_rate' => [
@@ -88,86 +90,62 @@ class Price extends Model {
                 'function'          => 'calcVatRate',
                 'description'       => 'VAT rate applied on the price (from accounting rule).',
                 'store'             => true,
-                'readonly'          => true
+                'readonly'          => true,
+                'visible'           => ['price_type', '=', 'direct']
+            ],
+
+            'test' => [
+                'type'              => 'integer',
+                'default'           => 5
             ]
 
         ];
     }
 
-    public static function calcName($om, $oids, $lang) {
+    public static function calcName($self) {
         $result = [];
-        $res = $om->read(__CLASS__, $oids, ['product_id.sku', 'price_list_id.name']);
-        if($res > 0 && count($res)) {
-            foreach($res as $oid => $odata) {
-                $result[$oid] = "{$odata['product_id.sku']} - {$odata['price_list_id.name']}";
-            }
+        $self->read(['product_id' => 'sku', 'price_list_id' => 'name']);
+        foreach($self as $id => $product) {
+            $result[$id] = "{$product['product_id']['sku']} - {$product['price_list_id']['name']}";
         }
         return $result;
     }
 
-    public static function calcVatRate($om, $oids, $lang) {
+    public static function calcVatRate($self) {
         $result = [];
-        $prices = $om->read(__CLASS__, $oids, ['accounting_rule_id.vat_rule_id.rate']);
-
-        if($prices > 0 && count($prices)) {
-            foreach($prices as $pid => $price) {
-                $result[$pid] = $price['accounting_rule_id.vat_rule_id.rate'];
-            }
+        $self->read(['accounting_rule_id' => ['vat_rule_id' => 'rate']]);
+        foreach($self as $id => $price) {
+            $result[$id] = $price['accounting_rule_id']['vat_rule_id']['rate'];
         }
         return $result;
     }
 
-    public static function calcPriceVat($om, $oids, $lang) {
+    public static function calcPriceVat($self) {
         $result = [];
-        $prices = $om->read(__CLASS__, $oids, ['price', 'vat_rate']);
-
-        if($prices > 0 && count($prices)) {
-            foreach($prices as $pid => $price) {
-                $result[$pid] = $price['price'] * (1.0 + $price['vat_rate']);
-            }
+        $self->read(['price', 'vat_rate']);
+        foreach($self as $id => $price) {
+            $result[$id] = round($price['price'] * (1.0 + $price['vat_rate']), 2);
         }
         return $result;
     }
 
-    public static function calcIsActive($om, $oids, $lang) {
+    public static function calcIsActive($self) {
         $result = [];
-        $prices = $om->read(__CLASS__, $oids, ['price_list_id.is_active']);
-
-        if($prices > 0 && count($prices)) {
-            foreach($prices as $pid => $price) {
-                $result[$pid] = $price['price_list_id.is_active'];
-            }
+        $self->read(['price_list_id' => 'is_active']);
+        foreach($self as $id => $price) {
+            $result[$id] = $price['price_list_id']['is_active'];
         }
         return $result;
     }
 
-    public static function onupdateAccountingRuleId($om, $oids, $values, $lang) {
-        $res = $om->write(__CLASS__, $oids, ['vat_rate' => null, 'price_vat' => null]);
-    }
+    public static function onchange($event, $self) {
+        $result = [];
 
-    public static function onupdatePriceListId($om, $oids, $values, $lang) {
-        $om->write(__CLASS__, $oids, ['name' => null], $lang);
-    }
-
-    public static function onupdateProductId($om, $oids, $values, $lang) {
-        $om->write(__CLASS__, $oids, ['name' => null], $lang);
-    }
-
-    /**
-     * Update price, based on VAT incl. price and applied VAT rate
-     */
-    public static function onupdatePriceVat($om, $oids, $values, $lang) {
-        $prices = $om->read(__CLASS__, $oids, ['price_vat', 'vat_rate']);
-
-        if($prices > 0 && count($prices)) {
-            foreach($prices as $pid => $price) {
-                $om->write(__CLASS__, $pid, ['price' => $price['price_vat'] / (1.0 + $price['vat_rate'])]);
-            }
+        if(isset($event['accounting_rule_id'])) {
+            $rule = AccountingRule::id($event['accounting_rule_id'])->read(['vat_rule_id' => 'rate'])->first();
+            $result['vat_rate'] = $rule['vat_rule_id']['rate'];
         }
-    }
-
-    public static function onupdatePrice($om, $oids, $values, $lang) {
-        $om->write(__CLASS__, $oids, ['price_vat' => null]);
+        return $result;
     }
 
     public function getUnique() {
