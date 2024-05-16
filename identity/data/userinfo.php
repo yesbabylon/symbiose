190a8bc2a482b80579b9f9b8fdd5c20ee7dd3d31
Cédric Françoys <cedricfrancoys@gmail.com>
2024-05-16 22:13:27 +0200

    refactored for using core_userinfo

--- identity/data/userinfo.php
+++ identity/data/userinfo.php
@@ -1,51 +1,50 @@
 <?php
 /*
     This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
-    Some Rights Reserved, Yesbabylon SRL, 2020-2021
+    Some Rights Reserved, Yesbabylon SRL, 2020-2024
     Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
 */
 use identity\User;
 
-list($params, $providers) = announce([
+list($params, $providers) = eQual::announce([
     'description'   => 'Returns descriptor of current User, based on received access_token',
+    'constants'     => ['AUTH_ACCESS_TOKEN_VALIDITY', 'BACKEND_URL'],
     'response'      => [
         'content-type'      => 'application/json',
         'charset'           => 'UTF-8',
         'accept-origin'     => '*'
     ],
-    'providers'     => ['context', 'orm', 'auth']
+    'providers'     => ['context', 'auth']
 ]);
 
-list($context, $om, $auth) = [$providers['context'], $providers['orm'], $providers['auth']];
+list($context, $auth) = [$providers['context'], $providers['auth']];
 
-// retrieve current User identifier (HTTP headers lookup through Authentication Manager)
-$user_id = $auth->userId();
-// make sure user is authenticated
-if($user_id <= 0) {
-    throw new Exception('user_unknown', QN_ERROR_NOT_ALLOWED);
-}
-// request directly the mapper to bypass permission check on User class
-$ids = $om->search('identity\User', ['id', '=', $user_id]);
-// make sure the User object is available
-if(!count($ids)) {
-    throw new Exception('unexpected_error', QN_ERROR_INVALID_USER);
-}
-// user has allways READ right on its own object
-$user = User::ids($ids)
+$result = eQual::run('get', 'core_userinfo');
+
+// user has always READ right on its own object
+$user = User::id($user_id)
     ->read([
-        'id',
-        'login',
-        'groups_ids'  => ['name', 'display_name'],
         'identity_id' => ['firstname', 'lastname'],
-        'language',
         'organisation_id'
     ])
-    ->adapt('txt')
+    ->adapt('json')
     ->first(true);
 
-$user['groups'] = array_values(array_map(function ($a) {return $a['name'];}, $user['groups_ids']));
+$result = array_merge($result, [
+        'identity_id'       => $user['identity_id'],
+        'organisation_id'   => $user['organisation_id']
+    ]);
+
+// renew JWT access token
+$access_token = $auth->token($user_id, constant('AUTH_ACCESS_TOKEN_VALIDITY'));
 
 // send back basic info of the User object
 $context->httpResponse()
-        ->body($user)
-        ->send();
+    ->cookie('access_token',  $access_token, [
+        'expires'   => time() + constant('AUTH_ACCESS_TOKEN_VALIDITY'),
+        'httponly'  => true,
+        'secure'    => constant('AUTH_TOKEN_HTTPS'),
+        'domain'    => parse_url(constant('BACKEND_URL'), PHP_URL_HOST)
+    ])
+    ->body($result)
+    ->send();
