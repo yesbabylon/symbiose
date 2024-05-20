b0ee808d47a8eca2b26a547dcc01d5dacb5b9bc0
Cédric Françoys <cedricfrancoys@gmail.com>
2024-05-20 15:54:18 +0200

    added support for SoftwareModel

---
+++ inventory/classes/SoftwareModel.class.php
@@ -0,0 +1,41 @@
+<?php
+/*
+    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
+    Some Rights Reserved, Yesbabylon SRL, 2020-2024
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+namespace inventory;
+
+use equal\orm\Model;
+
+class SoftwareModel extends Model {
+
+    public static function getColumns() {
+        return [
+
+            'name' => [
+                'type'              => 'string',
+                'description'       => "Name of the software.",
+                'required'          => true
+            ],
+
+            'description' => [
+                'type'              => 'string',
+                'usage'             => 'text/plain',
+                'description'       => 'Information about a software.'
+            ],
+
+            'edition' => [
+                'type'              => 'string',
+                'description'       => "Type of edition (CE/EE/Pro/...)."
+            ],
+
+            'version' => [
+                'type'              => 'string',
+                'description'       => "Installed version of the software."
+            ]
+
+        ];
+    }
+}
