e98260cb668b192340239b47e8c3e6347509fd84
alexandraYesbabylon <alexandra@yesbabylon.com>
2024-06-04 10:43:15 +0000

    update licenses symbiose in the collects files

--- inventory/data/service/service-collect.php
+++ inventory/data/service/service-collect.php
@@ -1,8 +1,8 @@
 <?php
 /*
-    This file is part of the Discope property management software.
-    Author: Yesbabylon SRL, 2020-2022
-    License: GNU AGPL 3 license <http://www.gnu.org/licenses/>
+    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
+    Some Rights Reserved, Yesbabylon SRL, 2020-2024
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
 */
 
 
