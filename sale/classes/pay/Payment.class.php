f8fe7eb387cb01d22ec9409755b76b40bd1decae
Lucas Laurent <lucas@yesbabylon.com>
2024-06-05 16:45:02 +0200

    Update funding and payment

--- sale/classes/pay/Payment.class.php
+++ sale/classes/pay/Payment.class.php
@@ -20,16 +20,17 @@ class Payment extends Model {
 
         return [
 
-            'partner_id' => [
+            'customer_id' => [
                 'type'              => 'many2one',
-                'foreign_object'    => 'identity\Partner',
-                'description'       => "The partner to whom the booking relates."
+                'foreign_object'    => 'sale\customer\Customer',
+                'description'       => "The customer to whom the payment relates."
             ],
 
             'amount' => [
                 'type'              => 'float',
                 'usage'             => 'amount/money:2',
-                'description'       => 'Amount paid (whatever the origin).'
+                'description'       => 'Amount paid (whatever the origin).',
+                'dependents'        => ['funding_id' => ['is_paid', 'paid_amount']]
             ],
 
             'communication' => [
@@ -56,13 +57,13 @@ class Payment extends Model {
             'payment_method' => [
                 'type'              => 'string',
                 'selection'         => [
-                    'voucher',              // gift, coupon, or tour-operator voucher
                     'cash',                 // cash money
                     'bank_card',            // electronic payment with bank (or credit) card
+                    'voucher',              // gift or coupon
                     'wire_transfer'         // transfer between bank account
                 ],
                 'description'       => "The method used for payment at the cashdesk.",
-                'visible'           => [ ['payment_origin', '=', 'cashdesk'] ],
+                'visible'           => ['payment_origin', '=', 'cashdesk'],
                 'default'           => 'cash'
             ],
 
@@ -70,20 +71,20 @@ class Payment extends Model {
                 'type'              => 'many2one',
                 'foreign_object'    => 'sale\pos\Operation',
                 'description'       => 'The operation the payment relates to.',
-                'visible'           => [ ['payment_origin', '=', 'cashdesk'] ]
+                'visible'           => ['payment_origin', '=', 'cashdesk']
             ],
 
             'statement_line_id' => [
                 'type'              => 'many2one',
                 'foreign_object'    => 'sale\pay\BankStatementLine',
                 'description'       => 'The bank statement line the payment relates to.',
-                'visible'           => [ ['payment_origin', '=', 'bank'] ]
+                'visible'           => ['payment_origin', '=', 'bank']
             ],
 
             'voucher_ref' => [
                 'type'              => 'string',
                 'description'       => 'The reference of the voucher the payment relates to.',
-                'visible'           => [ ['payment_method', '=', 'voucher'] ]
+                'visible'           => [ ['payment_origin', '=', 'cashdesk'], ['payment_method', '=', 'voucher'] ]
             ],
 
             'funding_id' => [
@@ -96,7 +97,8 @@ class Payment extends Model {
             'invoice_id' => [
                 'type'              => 'many2one',
                 'foreign_object'    => 'sale\accounting\invoice\Invoice',
-                'description'       => 'The invoice targeted by the payment, if any.'
+                'description'       => 'The invoice targeted by the payment, if any.',
+                'domain'            => ['status', '=', 'invoice']
             ],
 
             'is_exported' => [
@@ -118,149 +120,150 @@ class Payment extends Model {
         ];
     }
 
-    /**
-     * Assign partner_id and invoice_id from invoice relating to funding, if any.
-     * Force recomputing of target funding computed fields (is_paid and paid_amount).
-     *
-     */
-    public static function onupdateFundingId($om, $ids, $values, $lang) {
-        trigger_error("ORM::calling sale\pay\Payment::onupdateFundingId", QN_REPORT_DEBUG);
+    public static function onchange($event, $values) {
+        $result = [];
 
-        $payments = $om->read(get_called_class(), $ids, ['funding_id', 'partner_id', 'funding_id.due_amount', 'amount', 'statement_line_id']);
+        if(isset($event['payment_origin'])) {
+            switch($event['payment_origin']) {
+                case 'cashdesk':
+                    $result['statement_line_id'] = null;
+                    break;
+                case 'bank':
+                    $result['payment_method'] = 'cash';
+                    $result['operation_id'] = null;
+                    $result['voucher_ref'] = null;
+                    break;
+            }
+        }
 
-        if($payments > 0) {
-            $fundings_ids = [];
-            foreach($payments as $pid => $payment) {
+        if(isset($event['funding_id'])) {
+            $funding = Funding::id($event['funding_id'])
+                ->read(['type', 'due_amount', 'invoice_id' => ['customer_id' => ['name']]])
+                ->first();
 
-                if($payment['funding_id']) {
-                    // make sure a partner_id is assigned to the payment
-                    if(!$payment['partner_id']) {
-                        $fundings = $om->read('sale\booking\Funding', $payment['funding_id'], [
-                                'type',
-                                'due_amount',
-                                'booking_id.customer_id.id',
-                                'booking_id.customer_id.name',
-                                'invoice_id',
-                                'invoice_id.partner_id.id',
-                                'invoice_id.partner_id.name'
-                            ],
-                            $lang);
+            if(!is_null($funding)) {
+                if($funding['funding_type'] == 'invoice' && isset($funding['invoice_id']['customer_id']))  {
+                    $result['customer_id'] = [
+                        'id'   => $funding['invoice_id']['customer_id']['id'],
+                        'name' => $funding['invoice_id']['customer_id']['name']
+                    ];
+                }
 
-                        if($fundings > 0) {
-                            $funding = reset($fundings);
-                            $values = [
-                                'partner_id' => $funding['booking_id.customer_id.id']
-                            ];
-                            if($funding['type'] == 'invoice')  {
-                                $values['partner_id'] = $funding['invoice_id.partner_id.id'];
-                                $values['invoice_id'] = $funding['invoice_id'];
-                            }
-                            $om->update(get_called_class(), $pid, $values);
-                        }
-                    }
-
-                    $om->update('sale\pay\Funding', $payment['funding_id'], ['is_paid' => null, 'paid_amount' => null]);
-                    $fundings_ids[] = $payment['funding_id'];
+                if(isset($values['amount']) && $values['amount'] > $funding['due_amount']) {
+                    $result['amount'] = $funding['due_amount'];
                 }
             }
-            // force immediate re-computing of the is_paid field
-            $om->read('sale\pay\Funding', array_unique($fundings_ids), ['is_paid', 'paid_amount']);
         }
+
+        return $result;
     }
 
     /**
-     * Check wether the payment can be updated, and perform some additional operations if necessary.
-     * This method can be overridden to define a more precise set of tests.
-     *
-     * @param  Object   $om         ObjectManager instance.
-     * @param  Array    $ids       List of objects identifiers.
-     * @param  Array    $values     Associative array holding the new values to be assigned.
-     * @param  String   $lang       Language in which multilang fields are being updated.
-     * @return Array    Returns an associative array mapping fields with their error messages. En empty array means that object has been successfully processed and can be updated.
+     * Assign customer_id and invoice_id from invoice relating to funding, if any.
+     * Force recomputing of target funding computed fields (is_paid and paid_amount).
      */
-    public static function canupdate($om, $ids, $values, $lang='en') {
-        $payments = $om->read(self::getType(), $ids, ['state', 'is_exported', 'payment_origin', 'amount', 'statement_line_id.amount', 'statement_line_id.remaining_amount'], $lang);
-        foreach($payments as $pid => $payment) {
+    public static function onupdateFundingId($self, $values) {
+        trigger_error("ORM::calling sale\pay\Payment::onupdateFundingId", QN_REPORT_DEBUG);
+        file_put_contents(QN_LOG_STORAGE_DIR.'/tmp.log', 'onupdateFundingId'.PHP_EOL, FILE_APPEND | LOCK_EX);
+
+        $funding_fields = [
+            'funding_type',
+            'due_amount',
+            'invoice_id' => ['customer_id'],
+        ];
+
+        $self->read(['funding_id', 'customer_id', 'funding_id' => $funding_fields, 'amount', 'statement_line_id']);
+        foreach($self as $id => $payment) {
+            if($payment['funding_id']) {
+                // make sure a customer_id is assigned to the payment
+                if(is_null($payment['customer_id']) && isset($payment['funding_id']['invoice_id'])) {
+                    self::id($id)->update([
+                        'customer_id' => $payment['funding_id']['invoice_id']['customer_id'],
+                        'invoice_id'  => $payment['funding_id']['invoice_id']['id'],
+                    ]);
+                }
+
+                self::id($id)->update(['is_paid' => null, 'paid_amount' => null]);
+                $fundings_ids[] = $payment['funding_id'];
+            }
+        }
+
+        Funding::ids($fundings_ids)->read(['is_paid', 'paid_amount']);
+    }
+
+    public static function canupdate($self, $values) {
+        $self->read(['state', 'is_exported', 'payment_origin', 'amount', 'statement_line_id' => ['amount', 'remaining_amount']]);
+        foreach($self as $payment) {
             if($payment['is_exported']) {
                 return ['is_exported' => ['non_editable' => 'Once exported a payment can no longer be updated.']];
             }
-            if($payment['payment_origin'] == 'bank') {
-                if(isset($values['amount'])) {
-                    $sign_line = intval($payment['statement_line_id.amount'] > 0) - intval($payment['statement_line_id.amount'] < 0);
-                    $sign_payment = intval($values['amount'] > 0) - intval($values['amount'] < 0);
-                    // #memo - we prevent creating payment that do not decrease the remaining amount
-                    if($sign_line != $sign_payment) {
-                        return ['amount' => ['incompatible_sign' => "Payment amount ({$values['amount']}) and statement line amount ({$payment['statement_line_id.amount']}) must have the same sign."]];
+
+            $payment_origin = $values['payment_origin'] ?? $payment['payment_origin'];
+            if($payment_origin == 'bank' && isset($values['amount'])) {
+                $statement_line = $payment['statement_line_id'];
+                if(isset($values['statement_line_id'])) {
+                    $statement_line = BankStatementLine::id($values['statement_line_id'])
+                        ->read(['amount', 'remaining_amount'])
+                        ->first();
+                }
+
+                $sign_line = intval($statement_line['amount'] > 0) - intval($statement_line['amount'] < 0);
+                $sign_payment = intval($values['amount'] > 0) - intval($values['amount'] < 0);
+
+                // #memo - we prevent creating payment that do not decrease the remaining amount
+                if($sign_line != $sign_payment) {
+                    return ['amount' => ['incompatible_sign' => "Payment amount ({$values['amount']}) and statement line amount ({$statement_line['amount']}) must have the same sign."]];
+                }
+
+                // #memo - when state is still draft, it means that reconcile is made manually
+                if($payment['state'] == 'draft') {
+                    if(round($statement_line['amount'], 2) < 0) {
+                        if(round($statement_line['remaining_amount'] - $values['amount'], 2) > 0) {
+                            return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$statement_line['remaining_amount']}) (err#1)."]];
+                        }
                     }
-                    // #memo - when state is still draft, it means that reconcile is made manually
-                    if($payment['state'] == 'draft') {
-                        if(round($payment['statement_line_id.amount'], 2) < 0) {
-                            if(round($payment['statement_line_id.remaining_amount'] - $values['amount'], 2) > 0) {
-                                return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$payment['statement_line_id.remaining_amount']}) (err#1)."]];
-                            }
-                        }
-                        else {
-                            if(round($payment['statement_line_id.remaining_amount'] - $values['amount'], 2) < 0) {
-                                return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$payment['statement_line_id.remaining_amount']}) (err#2)."]];
-                            }
+                    else {
+                        if(round($statement_line['remaining_amount'] - $values['amount'], 2) < 0) {
+                            return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$statement_line['remaining_amount']}) (err#2)."]];
                         }
                     }
-                    else  {
-                        if(round($payment['statement_line_id.amount'], 2) < 0) {
-                            if(round($payment['statement_line_id.remaining_amount'] + $payment['amount'] - $values['amount'], 2) > 0) {
-                                return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$payment['statement_line_id.remaining_amount']}) (err#3)."]];
-                            }
+                }
+                else  {
+                    if(round($statement_line['amount'], 2) < 0) {
+                        if(round($statement_line['remaining_amount'] + $payment['amount'] - $values['amount'], 2) > 0) {
+                            return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$statement_line['remaining_amount']}) (err#3)."]];
                         }
-                        else {
-                            if(round($payment['statement_line_id.remaining_amount'] + $payment['amount'] - $values['amount'], 2) < 0) {
-                                return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$payment['statement_line_id.remaining_amount']}) (err#4)."]];
-                            }
+                    }
+                    else {
+                        if(round($statement_line['remaining_amount'] + $payment['amount'] - $values['amount'], 2) < 0) {
+                            return ['amount' => ['excessive_amount' => "Payment amount ({$values['amount']}) cannot be higher than statement line remaining amount ({$statement_line['remaining_amount']}) (err#4)."]];
                         }
                     }
                 }
             }
         }
-        return parent::canupdate($om, $ids, $values, $lang);
+
+        return parent::canupdate($self, $values);
     }
 
+    public static function ondelete($self) {
+        $self->read(['statement_line_id', 'funding_id']);
+        foreach($self as $payment) {
+            BankStatementLine::id($payment['statement_line_id'])->update(['status' => 'pending']);
+            Funding::id($payment['funding_id'])->update(['is_paid' => false]);
+        }
 
-    /**
-     * Hook invoked before object deletion for performing object-specific additional operations.
-     *
-     * @param  \equal\orm\ObjectManager     $om         ObjectManager instance.
-     * @param  array                        $oids       List of objects identifiers.
-     * @return void
-     */
-    public static function ondelete($om, $oids) {
-        // set back related statement line status to 'pending'
-        $payments = $om->read(__CLASS__, $oids, ['statement_line_id', 'funding_id']);
-        if($payments > 0) {
-            foreach($payments as $pid => $payment) {
-                $om->update('sale\pay\BankStatementLine', $payment['statement_line_id'], ['status' => 'pending']);
-                $om->update('sale\pay\Funding', $payment['funding_id'], ['is_paid' => false]);
+        return parent::ondelete($self);
+    }
+
+    public static function candelete($self) {
+        $self->read(['status']);
+        foreach($self as $payment) {
+            if($payment['status'] == 'paid') {
+                return ['status' => ['non_removable' => 'Paid payments cannot be removed.']];
             }
         }
-        return parent::ondelete($om, $oids);
+
+        return parent::candelete($self);
     }
-
-    /**
-     * Check wether the invoice can be deleted.
-     *
-     * @param  \equal\orm\ObjectManager    $om         ObjectManager instance.
-     * @param  array                       $ids        List of objects identifiers.
-     * @return array                       Returns an associative array mapping fields with their error messages. An empty array means that object has been successfully processed and can be deleted.
-     */
-    public static function candelete($om, $ids) {
-        $payments = $om->read(self::getType(), $ids, [ 'status' ]);
-
-        if($payments > 0) {
-            foreach($payments as $id => $payment) {
-                if($payment['status'] == 'paid') {
-                    return ['status' => ['non_removable' => 'Paid payments cannot be removed.']];
-                }
-            }
-        }
-        return parent::candelete($om, $ids);
-    }
-
 }
