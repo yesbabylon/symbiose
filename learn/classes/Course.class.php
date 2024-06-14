a6cf709eefc37f9ae57f160cc4caf451f37fd3b0
AlexisVS <alexis.vansan1440@gmail.com>
2024-06-14 18:26:49 +0200

    Working on UserAccess and UserStatus

--- learn/classes/Course.class.php
+++ learn/classes/Course.class.php
@@ -14,168 +14,168 @@ use equal\orm\ObjectManager;
 class Course extends Model
 {
 
-    public static function getColumns(): array
-    {
-        return [
-            'name' => [
-                'type'        => 'string',
-                'description' => 'Unique slug of the program.'
-            ],
+	public static function getColumns(): array
+	{
+		return [
+			'name' => [
+				'type'        => 'string',
+				'description' => 'Unique slug of the program.'
+			],
 
-            'title' => [
-                'type'      => 'string',
-                'multilang' => true
-            ],
+			'title' => [
+				'type'      => 'string',
+				'multilang' => true
+			],
 
-            'subtitle' => [
-                'type'      => 'string',
-                'multilang' => true
-            ],
+			'subtitle' => [
+				'type'      => 'string',
+				'multilang' => true
+			],
 
-            'description' => [
-                'type'      => 'string',
-                'usage'     => 'text/plain',
-                'multilang' => true
-            ],
+			'description' => [
+				'type'      => 'string',
+				'usage'     => 'text/plain',
+				'multilang' => true
+			],
 
-            'view_link' => [
-                'type'        => 'computed',
-                'description' => 'URL of the program page in view mode.',
-                'function'    => 'calcViewLink',
-                'result_type' => 'string',
-                'usage'       => 'uri/url',
-                'multilang'   => true
-            ],
+			'view_link' => [
+				'type'        => 'computed',
+				'description' => 'URL of the program page in view mode.',
+				'function'    => 'calcViewLink',
+				'result_type' => 'string',
+				'usage'       => 'uri/url',
+				'multilang'   => true
+			],
 
-            'edit_link' => [
-                'type'        => 'computed',
-                'description' => 'URL of the program page in edit mode.',
-                'function'    => 'calcEditLink',
-                'result_type' => 'string',
-                'usage'       => 'uri/url',
-                'multilang'   => true
-            ],
+			'edit_link' => [
+				'type'        => 'computed',
+				'description' => 'URL of the program page in edit mode.',
+				'function'    => 'calcEditLink',
+				'result_type' => 'string',
+				'usage'       => 'uri/url',
+				'multilang'   => true
+			],
 
-            'modules' => [
-                'type'  => 'alias',
-                'alias' => 'modules_ids'
-            ],
+			'modules' => [
+				'type'  => 'alias',
+				'alias' => 'modules_ids'
+			],
 
-            'modules_ids' => [
-                'type'           => 'one2many',
-                'foreign_object' => 'learn\Module',
-                'foreign_field'  => 'course_id',
-                'order'          => 'order',
-                'sort'           => 'asc',
-                'ondetach'       => 'delete',
-            ],
+			'modules_ids' => [
+				'type'           => 'one2many',
+				'foreign_object' => 'learn\Module',
+				'foreign_field'  => 'course_id',
+				'order'          => 'order',
+				'sort'           => 'asc',
+				'ondetach'       => 'delete',
+			],
 
-            'quizzes_ids' => [
-                'type'           => 'one2many',
-                'foreign_object' => 'learn\Quiz',
-                'foreign_field'  => 'course_id',
-                'ondetach'       => 'delete'
-            ],
+			'quizzes_ids' => [
+				'type'           => 'one2many',
+				'foreign_object' => 'learn\Quiz',
+				'foreign_field'  => 'course_id',
+				'ondetach'       => 'delete'
+			],
 
-            'bundles_ids' => [
-                'type'           => 'one2many',
-                'foreign_object' => 'learn\Bundle',
-                'foreign_field'  => 'course_id',
-                'ondetach'       => 'delete'
-            ],
+			'bundles_ids' => [
+				'type'           => 'one2many',
+				'foreign_object' => 'learn\Bundle',
+				'foreign_field'  => 'course_id',
+				'ondetach'       => 'delete'
+			],
 
-            'langs_ids' => [
-                'type'            => 'many2many',
-                'foreign_object'  => 'learn\Lang',
-                'foreign_field'   => 'courses_ids',
-                'rel_table'       => 'learn_rel_lang_course',
-                'rel_foreign_key' => 'lang_id',
-                'rel_local_key'   => 'course_id',
-                'description'     => "List of languages in which the program is available"
-            ]
+			'langs_ids' => [
+				'type'            => 'many2many',
+				'foreign_object'  => 'learn\Lang',
+				'foreign_field'   => 'courses_ids',
+				'rel_table'       => 'learn_rel_lang_course',
+				'rel_foreign_key' => 'lang_id',
+				'rel_local_key'   => 'course_id',
+				'description'     => "List of languages in which the program is available"
+			]
 
-        ];
-    }
+		];
+	}
 
-    /**
-     * @param ObjectManager $om
-     * @param array $oids
-     * @param $lang
-     * @return array
-     */
-    public static function calcViewLink(ObjectManager $om, array $oids, $lang): array
-    {
-        $result = [];
+	/**
+	 * @param ObjectManager $om
+	 * @param array $oids
+	 * @param $lang
+	 * @return array
+	 */
+	public static function calcViewLink(ObjectManager $om, array $oids, $lang): array
+	{
+		$result = [];
 
-        $courses = $om->read(__CLASS__, $oids, [
-            'id',
-            'title'
-        ], $lang);
+		$courses = $om->read(__CLASS__, $oids, [
+			'id',
+			'title'
+		], $lang);
 
-        foreach ($courses as $oid => $course) {
-            $id = self::formatLinkIdNumber($course['id']);
-            $title = self::createSlug($course['title']);
+		foreach ($courses as $oid => $course) {
+			$id = self::formatLinkIdNumber($course['id']);
+			$title = self::createSlug($course['title']);
 
-            $result[$oid] = '/learning/#/course/' . $id . '/' . $title;
-        }
+			$result[$oid] = '/learning/#/course/' . $id . '/' . $title . '?mode=view' . '&lang=' . $lang;
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    /**
-     * @param ObjectManager $om
-     * @param array $oids
-     * @param $lang
-     * @return array
-     */
-    public static function calcEditLink(ObjectManager $om, array $oids, $lang): array
-    {
-        $result = [];
+	/**
+	 * @param ObjectManager $om
+	 * @param array $oids
+	 * @param $lang
+	 * @return array
+	 */
+	public static function calcEditLink(ObjectManager $om, array $oids, $lang): array
+	{
+		$result = [];
 
-        $courses = $om->read(__CLASS__, $oids, [
-            'id',
-            'title'
-        ], $lang);
+		$courses = $om->read(__CLASS__, $oids, [
+			'id',
+			'title'
+		], $lang);
 
-        foreach ($courses as $oid => $course) {
-            $id = self::formatLinkIdNumber($course['id']);
-            $title = self::createSlug($course['title']);
+		foreach ($courses as $oid => $course) {
+			$id = self::formatLinkIdNumber($course['id']);
+			$title = self::createSlug($course['title']);
 
-            $result[$oid] = '/learning/#/course/' . $id . '/' . $title . '?mode=edit';
-        }
+			$result[$oid] = '/learning/#/course/' . $id . '/' . $title . '?mode=edit';
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    /**
-     * Convert a sentence to a slug
-     *
-     * @param $title
-     * @return string
-     */
-    public static function createSlug($title): string
-    {
-        // Convert the title to lowercase
-        $slug = strtolower($title);
+	/**
+	 * Convert a sentence to a slug
+	 *
+	 * @param $title
+	 * @return string
+	 */
+	public static function createSlug($title): string
+	{
+		// Convert the title to lowercase
+		$slug = strtolower($title);
 
-        // Replace any non-alphanumeric characters (except for hyphens) with spaces
-        $slug = preg_replace('/[^a-z0-9-]+/', ' ', $slug);
+		// Replace any non-alphanumeric characters (except for hyphens) with spaces
+		$slug = preg_replace('/[^a-z0-9-]+/', ' ', $slug);
 
-        // Replace spaces with hyphens
-        $slug = str_replace(' ', '-', $slug);
+		// Replace spaces with hyphens
+		$slug = str_replace(' ', '-', $slug);
 
-        // Remove any leading or trailing hyphens
-        return trim($slug, '-');
-    }
+		// Remove any leading or trailing hyphens
+		return trim($slug, '-');
+	}
 
-    /**
-     * Format a number to an 8-digit string
-     *
-     * @param $num
-     * @return string
-     */
-    public static function formatLinkIdNumber($num): string
-    {
-        return str_pad($num, 8, '0', STR_PAD_LEFT);
-    }
+	/**
+	 * Format a number to an 8-digit string
+	 *
+	 * @param $num
+	 * @return string
+	 */
+	public static function formatLinkIdNumber($num): string
+	{
+		return str_pad($num, 8, '0', STR_PAD_LEFT);
+	}
 }
--- learn/classes/Module.class.php
+++ learn/classes/Module.class.php
@@ -14,168 +14,169 @@ use equal\orm\ObjectManager;
 class Module extends Model
 {
 
-    public static function getColumns()
-    {
-        return [
-            'identifier' => [
-                'type'        => 'integer',
-                'description' => 'Unique identifier the module within the course.',
-                'default'     => 1
-            ],
+	public static function getColumns(): array
+	{
+		return [
+			'identifier' => [
+				'type'        => 'integer',
+				'description' => 'Unique identifier the module within the course.',
+				'default'     => 1
+			],
 
-            'order' => [
-                'type'        => 'integer',
-                'description' => 'Position of the module in the course.'
-            ],
+			'order' => [
+				'type'        => 'integer',
+				'description' => 'Position of the module in the course.'
+			],
 
-            'name' => [
-                'type'  => 'alias',
-                'alias' => 'title'
-            ],
+			'name' => [
+				'type'  => 'alias',
+				'alias' => 'title'
+			],
 
-            'title' => [
-                'type'        => 'string',
-                'required'    => true,
-                'description' => "Description of the module as presented to user.",
-                'multilang'   => true
-            ],
+			'title' => [
+				'type'        => 'string',
+				'required'    => true,
+				'description' => "Description of the module as presented to user.",
+				'multilang'   => true
+			],
 
-            'link' => [
-                'type'        => 'computed',
-                'description' => "URL to visual editor of the module.",
-                'function'    => 'calcLink',
-                'result_type' => 'string',
-                'usage'       => 'uri/url',
-                'store'       => true,
-                'multilang'   => true
-            ],
+			'link' => [
+				'type'        => 'computed',
+				'description' => "URL to visual editor of the module.",
+				'function'    => 'calcLink',
+				'result_type' => 'string',
+				'usage'       => 'uri/url',
+				'multilang'   => true
+			],
 
-            'page_count' => [
-                'type'        => 'computed',
-                'description' => "Total amount of pages in the module.",
-                'function'    => 'calcPageCount',
-                'result_type' => 'integer',
-                'store'       => true
-            ],
+			'page_count' => [
+				'type'        => 'computed',
+				'description' => "Total amount of pages in the module.",
+				'function'    => 'calcPageCount',
+				'result_type' => 'integer',
+				'store'       => true
+			],
 
-            'chapter_count' => [
-                'type'        => 'computed',
-                'description' => "Total amount of chapters in the module.",
-                'function'    => 'calcChapterCount',
-                'result_type' => 'integer',
-                'store'       => true
-            ],
+			'chapter_count' => [
+				'type'        => 'computed',
+				'description' => "Total amount of chapters in the module.",
+				'function'    => 'calcChapterCount',
+				'result_type' => 'integer',
+				'store'       => true
+			],
 
-            'description' => [
-                'type'      => 'string',
-                'usage'     => 'text/plain',
-                'multilang' => true
-            ],
+			'description' => [
+				'type'      => 'string',
+				'usage'     => 'text/plain',
+				'multilang' => true
+			],
 
-            'duration' => [
-                'type'        => 'computed',
-                'description' => "Total duration of chapters in the module.",
-                'function'    => 'calcChaptersDuration',
-                'result_type' => 'integer'
-            ],
+			'duration' => [
+				'type'        => 'computed',
+				'description' => "Total duration of chapters in the module.",
+				'function'    => 'calcChaptersDuration',
+				'result_type' => 'integer'
+			],
 
-            'chapters' => [
-                'type'  => 'alias',
-                'alias' => 'chapters_ids'
-            ],
+			'chapters' => [
+				'type'  => 'alias',
+				'alias' => 'chapters_ids'
+			],
 
-            'chapters_ids' => [
-                'type'           => 'one2many',
-                'foreign_object' => 'learn\Chapter',
-                'foreign_field'  => 'module_id',
-                'order'          => 'order',
-                'sort'           => 'asc',
-                'ondetach'       => 'delete',
-                'onupdate'       => 'onupdateChaptersIds'
-            ],
+			'chapters_ids' => [
+				'type'           => 'one2many',
+				'foreign_object' => 'learn\Chapter',
+				'foreign_field'  => 'module_id',
+				'order'          => 'order',
+				'sort'           => 'asc',
+				'ondetach'       => 'delete',
+				'onupdate'       => 'onupdateChaptersIds'
+			],
 
-            'course_id' => [
-                'type'           => 'many2one',
-                'foreign_object' => 'learn\Course',
-                'description'    => 'Course the module relates to.',
-                'ondelete'       => 'cascade'
-                // delete module when parent course is deleted
-            ]
-        ];
-    }
+			'course_id' => [
+				'type'           => 'many2one',
+				'foreign_object' => 'learn\Course',
+				'description'    => 'Course the module relates to.',
+				'ondelete'       => 'cascade'
+				// delete module when parent course is deleted
+			]
+		];
+	}
 
-    public static function calcLink(ObjectManager $om, $oids, $lang): array
-    {
-        $result = [];
+	public static function calcLink(ObjectManager $om, $oids, $lang): array
+	{
+		$result = [];
 
-        foreach ($oids as $oid) {
-            $course_id = $om->read(__CLASS__, $oid, ['course_id'], $lang)['course_id'];
-            $course_title = $om->read('learn\Course', $course_id, ['title'], $lang)['title'];
-            $result[$oid] =
-                '/learning/#/course/' . Course::formatLinkIdNumber($course_id) .
-                '/' . Course::createSlug($course_title) .
-                '?mode=edit&module=' . $oid .
-                '&lang=' . $lang;
-        }
+		foreach ($oids as $oid) {
+			$course_id = ($om->read(__CLASS__, $oid, ['course_id'], $lang))['course_id'];
 
-        return $result;
-    }
+			$course_title = $om->read('learn\Course', $course_id, ['title'], $lang)['title'];
+			$result[$oid] =
+				'/learning/#/course/' . Course::formatLinkIdNumber($course_id) .
+				'/' . Course::createSlug($course_title) .
+				'?mode=edit' .
+				'&module=' . $oid .
+				'&lang=' . $lang;
+		}
+
+		return $result;
+	}
 
 
-    public static function calcPageCount($om, $oids, $lang)
-    {
-        $result = [];
+	public static function calcPageCount($om, $oids, $lang): array
+	{
+		$result = [];
 
-        $modules = $om->read(__CLASS__, $oids, ['chapters_ids'], $lang);
+		$modules = $om->read(__CLASS__, $oids, ['chapters_ids'], $lang);
 
-        foreach ($modules as $oid => $module) {
-            $chapters = $om->read('learn\Chapter', $module['chapters_ids'], ['page_count'], $lang);
-            $result[$oid] = 0;
-            foreach ($chapters as $chapter) {
-                $result[$oid] += $chapter['page_count'];
-            }
-        }
+		foreach ($modules as $oid => $module) {
+			$chapters = $om->read('learn\Chapter', $module['chapters_ids'], ['page_count'], $lang);
+			$result[$oid] = 0;
+			foreach ($chapters as $chapter) {
+				$result[$oid] += $chapter['page_count'];
+			}
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public static function calcChapterCount($om, $oids, $lang)
-    {
-        $result = [];
+	public static function calcChapterCount($om, $oids, $lang)
+	{
+		$result = [];
 
-        $modules = $om->read(__CLASS__, $oids, ['chapters_ids'], $lang);
+		$modules = $om->read(__CLASS__, $oids, ['chapters_ids'], $lang);
 
-        foreach ($modules as $oid => $module) {
-            $result[$oid] = count($module['chapters_ids']);
-        }
+		foreach ($modules as $oid => $module) {
+			$result[$oid] = count($module['chapters_ids']);
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public static function onupdateChaptersIds($orm, $oids, $values, $lang)
-    {
-        // force immediate refresh chapter_count
-        $orm->write(__CLASS__, $oids, ['chapter_count' => null], $lang);
-        $orm->read(__CLASS__, $oids, ['chapter_count'], $lang);
-    }
+	public static function onupdateChaptersIds($orm, $oids, $values, $lang)
+	{
+		// force immediate refresh chapter_count
+		$orm->write(__CLASS__, $oids, ['chapter_count' => null], $lang);
+		$orm->read(__CLASS__, $oids, ['chapter_count'], $lang);
+	}
 
-    public static function calcChaptersDuration($self): array
-    {
-        $result = [];
-        /** @var $self Collection */
-        $self->read(['chapters_ids' => ['duration']]);
+	public static function calcChaptersDuration($self): array
+	{
+		$result = [];
+		/** @var $self Collection */
+		$self->read(['chapters_ids' => ['duration']]);
 
-        foreach ($self as $id => $module) {
-            $moduleDurationCount = 0;
+		foreach ($self as $id => $module) {
+			$moduleDurationCount = 0;
 
-            foreach ($module['chapters_ids'] as $chapter) {
-                $moduleDurationCount += $chapter['duration'];
-            }
+			foreach ($module['chapters_ids'] as $chapter) {
+				$moduleDurationCount += $chapter['duration'];
+			}
 
-            $result[$id] = $moduleDurationCount;
-        }
+			$result[$id] = $moduleDurationCount;
+		}
 
-        return $result;
-    }
+		return $result;
+	}
 
 }
