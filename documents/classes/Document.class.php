1ef89f26d3d4199edbc58c05bccb053686bf7d5a
Cédric Françoys <cedricfrancoys@gmail.com>
2024-07-13 09:52:04 +0200

    added assignments (roles)

--- documents/classes/Document.class.php
+++ documents/classes/Document.class.php
@@ -118,8 +118,34 @@ class Document extends Model {
                 'foreign_object'    => 'communication\template\TemplateAttachment',
                 'foreign_field'     => 'document_id',
                 'description'       => "The links between document and templates."
-            ]
+            ],
 
+            'assignments_ids' => [
+                'type'              => 'one2many',
+                'foreign_object'    => 'core\Assignment',
+                'foreign_field'     => 'object_id',
+                'domain'            => [ ['object_class', '=', 'documents\Document'], ['object_id', '=', 'object.id'] ],
+                'description'       => "Links between document and related roles assignments."
+            ]
+        ];
+    }
+
+    public static function getRoles() {
+        return [
+            "owner" => [
+                "description" => "Creator of the document. Has full control over it.",
+                "rights" => EQ_R_READ | EQ_R_WRITE | EQ_R_DELETE | EQ_R_MANAGE
+            ],
+            "editor" => [
+                "description" => "Editor of the document. Has read and write access on it. Cannot remove it.",
+                "implied_by" => ['owner'],
+                "rights" => EQ_R_READ | EQ_R_WRITE
+            ],
+            "viewer" => [
+                "description" => "Viewer of the document. Cannot edit the content.",
+                "implied_by" => ['editor'],
+                "rights" => EQ_R_READ
+            ]
         ];
     }
 
@@ -186,7 +212,7 @@ class Document extends Model {
      *
      * @return string | bool    In case of success, the extension is returned. If no extension matches the content type, the method returns false.
      */
-    public static function _getExtensionFromType($content_type, $name = '') {
+    public static function computeExtensionFromType($content_type, $name = '') {
 
         static $map_extensions = [
             '3g2'   =>	'video/3gpp2',
@@ -369,13 +395,10 @@ class Document extends Model {
         // if we have a name holding an extension, check if the given content_type is amongst the valid MIME of the map
         if(strlen($name)) {
             $extension = strtolower( ( ($n = strrpos($name, ".")) === false) ? "" : substr($name, $n+1) );
-            if(strlen($extension)) {
-                if(isset($map_extensions[$extension])) {
-                    $map = (array) $map_extensions[$extension];
-                    foreach($map as $mime) {
-                        if($mime == $content_type) {
-                            return $extension;
-                        }
+            if(strlen($extension) && isset($map_extensions[$extension])) {
+                foreach((array) $map_extensions[$extension] as $mime) {
+                    if($mime == $content_type) {
+                        return $extension;
                     }
                 }
             }
@@ -472,7 +495,7 @@ class Document extends Model {
                 // unknown image type : fallback to hardcoded default thumbnail
                 $result[$id] = base64_decode('/9j/4AAQSkZJRgABAQEAYABgAAD/2wBDAAIBAQEBAQIBAQECAgICAgQDAgICAgUEBAMEBgUGBgYFBgYGBwkIBgcJBwYGCAsICQoKCgoKBggLDAsKDAkKCgr/2wBDAQICAgICAgUDAwUKBwYHCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgr/wAARCACWAJYDAREAAhEBAxEB/8QAHgABAQACAgMBAQAAAAAAAAAAAAkHCAQGAgUKAwH/xABKEAAABAMDAw4MBQMDBQAAAAAAAQIEAwUGBwgRCQoSFxkhNThVWHR3k5e00dMTFBUxOVRXkZWytdIWQZKm1CIyURgjlkJhcYGE/8QAGQEBAAMBAQAAAAAAAAAAAAAAAAIDBAEF/8QAIREBAAEDBAMBAQAAAAAAAAAAAAECAxESEzJRBDFBMyH/2gAMAwEAAhEDEQA/AL+AAAAxbUF+S5XSdQPKTqi93ZjLppLo3gZhLX1dy+FHbRMCPQiQ1RiUhWBkeBkR7IlFFc+ocmqmPcuJrgNxHhpWUdIct74d27nUua6OzXAbiPDSso6Q5b3wbdzqTXR2a4DcR4aVlHSHLe+DbudSa6OzXAbiPDSso6Q5b3wbdzqTXR2a4DcR4aVlHSHLe+DbudSa6OzXAbiPDSso6Q5b3wbdzqTXR2a4DcR4aVlHSHLe+DbudSa6OzXAbiPDSso6Q5b3wbdzqTXR29rRF8a6PaZU7eirOb0dnc/nLvHxSUyatGLpzGwwx0IUOKalecvMX5jk0VU+4diqmfUskCLoAAAAAAAAAnNnFF8O0qwaxOk7ArMZo9lMW0qJMfLk4ZrSlXk5qiCiKzJX9yDjKdw8VJwPQhLTjgsyPR49uK68z8VXqppp/iVl2zJ/3vb3tOzCr7vNj0aoJbLXvij16c4ZM0Ij6JLNBG6jQ9NWipJno44aRY+chtmuij+SzU0VVemSNZGyn3Bk/ekl/mjm7b7S2rnRrI2U+4Mn70kv80N232bVzo1kbKfcGT96SX+aG7b7Nq50ayNlPuDJ+9JL/NDdt9m1c6NZGyn3Bk/ekl/mhu2+zaudGsjZT7gyfvSS/wA0N232bVzo1kbKfcGT96SX+aG7b7Nq508Y2RLynUCEqPFuzYJQk1KP8ZyXYIv/ALA3bfZtXOmrjV1MpLNIMxlz2OzesXKIzV01jHDit40NRKREQtJkaFpURGSkmRkZEZGJzEVRiUImYnMLbZHvLLye8zL5bdmvSVE2Y2lwEE3kU8jkUKDVaEpPAtgiTDekkv6oewUXZXDL+5CPPvWZtzmPTVbuRXGJ9qJihaAAAAAAAAkZnRe31h/E6k+eVjZ4nuWe/wDGTM2/3GVWcpTnqDESvc0rHFQcUrgAAAAAAAHHm21Tni6/lMB8xdidn8stYvEUjZZO3cZuyqWtGEqdx2xl4SHCcO4cFakYkZaREszLEjLEhumcU5YIjM4d9vv3GLbbhFrf4Vrhu4XLYrk49KVax0kQnyEKJSVoWk8YUZB6JqRiSkqIjLEjSo+U1U3KUqqardSnmR2yzsuvANJZdcvZ1RAaWgw/BtaYqZ0ZQ4VUFhgmFEP+1D0sCLDYKPiRoLTxSMN6zNucx6aLdyKv5PtSMULQAAAAAASMzovb6w/idSfPKxs8T3LPf+MmZt/uMqs5SnPUGIle5pWOKg4pXAAAAAAAAOPNtqnPF1/KYD5qboG7Wsu5UpJ9SgjdVwlgp5Q+iG8pdrsmvYWTTCxy2OnkvpW+TpQoqMEx2UciMkOIK8P6IicTwPzGRmRkZGZHipqmmcw3VUxVGJfPxfruSWnXELbo1m9XLjuZfFUbmmKlgQVwob+ASiNK0K/6YqD0SWkjM0KwMjwNJnspqi5Sx1UzRUp9kVssA7vEJZ3SL0VQQjrpo0MqVqh3GSg6kgwyLFvFxMtJ6hJGrFJf7sNClYaaFmrDes7c5j00W7mqMT7UmFC0AAAAASMzovb6w/idSfPKxs8T3LPf+MmZt/uMqs5SnPUGIle5pWOKg4pXAAAAAAAAP4pKVpNC0kZGWBkf5gPnhv73I7b7g94V7MoEkmbenCnhvqJq9ohRwlIKJ4SCXhU7EOPDPAjSrBWKdIiMjIz20VxXSxV0TRU/eHlispRChphJvTTQySkiI1SeXqP/ANmbfE//ACYbVHRu19uk283/AC9xeepGHQtvNr0SpJXAcpcN27yTMkKgxS2NJESHAStB4bB4KLEtg8SHYoppnMOTXVVGJYiZPX0sfN5rK30dq7aR0R2jprGVDiwIqFEpERC0mSkLSoiUSiMjIyIyPEh2YiYxKMTiX0hZKC3+0S85cEoC2G1iZePVC8bPGcymBpSSnimj2O0KOskpSklrTBSpWBEWko8B5d2mKK5iG2iqaqctiRBMAAABIzOi9vrD+J1J88rGzxPcs9/4yZm3+4yqzlKc9QYiV7mlY4qDilcAAAAAAAAAONNpNKJ+wiSqeyps9axk6MVs7gJiQ1l/g0qIyMgHVDu33eDPE7BaL/4s07sdzKOmno/033d/YLRf/FmndhmTTT0jHl+6Mo+hb7kuk1E0pLZOzVQbGKppKmMNvCNZuHRGrRhkRYmRFs4Y7BDVZmZoZr0RFf8AFMcg56Laznjk9+tPRiv/AKyvtcIbfilYAAAAkZnRe31h/E6k+eVjZ4nuWe/8ZMzb/cZVZylOeoMRK9zSscVBxSuAAAAAAAAAAAAAEQc4k3d0t5PmHWHY12eDJe5qP5Bz0W1nPHJ79aejFf8A1lfa4Q2/FKwAAABIzOi9vrD+J1J88rGzxPcs9/4yZm3+4yqzlKc9QYiV7mlY4qDilc4M+n7KnmZO3hKVpK0UIQWyox2ImZcmcPS6qcr3sce9PaJ7cuaoNVOV72OPentDbk1Qaqcr3sce9PaG3Jqg1U5XvY496e0NuTVBqpyvexx709obcmqDVTle9jj3p7Q25NUP3llo0pmL2GyU1jQjiqJKFqwMsT8xbA5NExDuqHYRB0ARBziTd3S3k+YdYdjXZ4Ml7mo/kHPRbWc8cnv1p6MV/wDWV9rhDb8UrAAAAEjM6L2+sP4nUnzysbPE9yz3/jJmbf7jKrOUpz1BiJXuaVjioOKVzgz6QsqhZkzeGpOirSQtB7KTHYmYcmMvSalkt30j/pSJ7kuaTUslu+kf9KQ3JNJqWS3fSP8ApSG5JpNSyW76R/0pDck0mpZLd9I/6UhuSaTUslu+kf8ASkNyTS5Ers5lUtfQ3ynUWKcJRKQhWBFiXmM8Bya5mCKXYRBIARBziTd3S3k+YdYdjXZ4Ml7mo/kHPRbWc8cnv1p6MV/9ZX2uENvxSsAAAASMzovb6w/idSfPKxs8T3LPf+MmZt/uMqs5SnPUGIle5pWOKg4pXAAAAAAAAAAAAACIOcSbu6W8nzDrDsa7PBkvc1H8g56Laznjk9+tPRiv/rK+1wht+KVgAAACRmdF7fWH8TqT55WNnie5Z7/xkzNv9xlVnKU56gxEr3NKxxUHFK56at54/kUrS4l6C04kTROIpOJILDESpiJlyfTqWqBVPr6eZT2CzRShmTVAqn19PMp7A0UmZNUCqfX08ynsDRSZk1QKp9fTzKewNFJmTVAqn19PMp7A0UmZNUCqfX08ynsDRSZlzJBXdROZu3auYiYyIsUkKQUMiPAz8+x/gcmiIh2JnLvQqTAEQc4k3d0t5PmHWHY12eDJe5qP5Bz0W1nPHJ79aejFf/WV9rhDb8UrAAAAEjM6L2+sP4nUnzysbPE9yz3/AIyZm3+4yqzlKc9QYiV7mlY4qDilc8Y0GC4hnBjwkrQrzpWnEjAcbyDJN6G3MJ7B3MuYg8gyTehtzCewMyYg8gyTehtzCewMyYg8gyTehtzCewMyYg8gyTehtzCewMyYg8gyTehtzCewMyYh5t5VLGkTwzWXwYa8MNJEIiMMzLrkDgAIg5xJu7pbyfMOsOxrs8GS9zUfyDnotrOeOT3609GK/wDrK+1wht+KVgAAACRmdF7fWH8TqT55WNnie5Z7/wAZMzb/AHGVWcpTnqDESvc0rHFQcUrn8UpKEmpaiIi85mYD8/HmXrkLnCHcSHjzL1yFzhBiQ8eZeuQucIMSHjzL1yFzhBiQ8eZeuQucIMSHjzL1yFzhBiR5Q3LeMrRhR0KP/CVEY4PMAARBziTd3S3k+YdYdjXZ4Ml7mo/kHPRbWc8cnv1p6MV/9ZX2uENvxSsAAAASMzovb6w/idSfPKxs8T3LPf8AjJmbf7jKrOUpz1BiJXuaVjioOKVz0ldyqaTaUpgSsjUaYpKiQyVhpFgYlTMRP9cn06d+DKo3ni+8u0W6qUMSfgyqN54vvLtDVSYk/BlUbzxfeXaGqkxJ+DKo3ni+8u0NVJiT8GVRvPF95doaqTEn4MqjeeL7y7Q1UmJc2naSqdtOm7lTJcBMOKSlxFKIv6fzLz7OJbAjVVTMOxE5d/FSYAiDnEm7ulvJ8w6w7GuzwZL3NR/IOei2s545PfrT0Yr/AOsr7XCG34pWAAAAJGZ0Xt9YfxOpPnlY2eJ7lnv/ABkzNv8AcZVZylOeoMRK9zSscVBxSuAAAAAAAAAAAAAEQc4k3d0t5PmHWHY12eDJe5qP5Bz0W1nPHJ79aejFf/WV9rhDb8UrAAAAEjM6L2+sP4nUnzysbPE9yz3/AIyZm3+4yqzlKc9QYiV7mlY4qDilcAAAAAAAAAAAAAIg5xJu7pbyfMOsOxrs8GS9zUfyDnotrOeOT3609GK/+sr7XCG34pWAAAAJFZ0WtBVDYfDNRaRsqlMi/wC2nK+0a/E9yz3/AI7bm7lrFl9MXU6vpOprRJJLZmivorpTCYTOFBi+AWyaoREJK1EZpNUNZYl+aTE70TrdszGlQLVpsc9rNM/Hm/3irErswatNjntZpn483+8MSZg1abHPazTPx5v94YkzBq02Oe1mmfjzf7wxJmDVpsc9rNM/Hm/3hiTMGrTY57WaZ+PN/vDEmYNWmxz2s0z8eb/eGJMwatNjntZpn483+8MSZg1abHPazTPx5v8AeGJMwatNjntZpn483+8MSZg1abHPazTPx5v94YkzCKOX3ryiq9vzwHdD1ZLpvBY0Sxau40tdojohRyiuFnDNSDMtIkrQZljsaRDVZiYoZb0xNameQaWiJktbOjQojIns+I8P8lOnxGMN/wDWV9rhDcEVLAAAAGrOVgydTLKE2DNpJTkxay2uKVcRX1IzJ2n/AGoprh6MVlGURGaIUbRhmayIzSuDDVskSkqstXJt1ZQroiuMIfVLk47/ABS1TTClX1zi0txHlzlUGK5llFPnTWKZeZcKPChKhxUGRkZKSoy/I8DIyL0IvW5jOWWbdcT6cPW/r93AttX6PJl3I7u2+4c0V9Gt/X7uBbav0eTLuQ3bfcGivo1v6/dwLbV+jyZdyG7b7g0V9Gt/X7uBbav0eTLuQ3bfcGivo1v6/dwLbV+jyZdyG7b7g0V9Gt/X7uBbav0eTLuQ3bfcGivo1v6/dwLbV+jyZdyG7b7g0V9Gt/X7uBbav0eTLuQ3bfcGivo1v6/dwLbV+jyZdyG7b7g0V9Gt/X7uBbav0eTLuQ3bfcGivo1v6/dwLbV+jyZdyG7b7g0V9O+XbckdfpvD2otrP31glU0QwJUNc0qWtqady9o0gGrBSkeGhoNxEJJKMoaNkzIiUaCURiFd+imP5OUqbVVU/wBX+u03fqEusWEUxd/s3gLTKaYlaGsKNFSRRHUXE1xnMTRwLwkWKpcVWBEWlEPAiLAi86qZqnMtcRERh3kcdAAAAAAAAAAAAAAAAAAAAAAAAAAB/9k=');
 
-                $extension = self::_getExtensionFromType($document['type'], $document['name']);
+                $extension = self::computeExtensionFromType($document['type'], $document['name']);
                 if($extension) {
                     $filename = EQ_BASEDIR.'/packages/documents/assets/img/extensions/'.$extension.'.jpg';
                     if(!is_file($filename)) {
