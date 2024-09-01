b16cfc83fbed668f77262e80b4016509de9c1c27
Cédric Françoys <cedricfrancoys@gmail.com>
2024-09-01 18:01:25 +0200

    set product name as description

--- sale/actions/receivablequeue/invoice.php
+++ sale/actions/receivablequeue/invoice.php
@@ -81,7 +81,7 @@ foreach($receivables_queues as $receivables_queue) {
             'name',
             'description',
             'customer_id',
-            'product_id',
+            'product_id' => ['id', 'name'],
             'price_id',
             'unit_price',
             'vat_rate',
@@ -119,10 +119,10 @@ foreach($receivables_queues as $receivables_queue) {
     foreach($receivables as $receivable) {
         $invoice_line = InvoiceLine::create([
                 'name'                  => $receivable['name'],
-                'description'           => $receivable['description'],
+                'description'           => $receivable['product_id']['name'],
                 'invoice_line_group_id' => $invoice_line_group['id'],
                 'invoice_id'            => $invoice['id'],
-                'product_id'            => $receivable['product_id'],
+                'product_id'            => $receivable['product_id']['id'],
                 'price_id'              => $receivable['price_id'],
                 'unit_price'            => $receivable['unit_price'],
                 'vat_rate'              => $receivable['vat_rate'],
