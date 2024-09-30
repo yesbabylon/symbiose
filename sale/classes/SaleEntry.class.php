0266f2f362546122e953a7810a47a492199125d5
Cédric Françoys <cedricfrancoys@gmail.com>
2024-09-30 12:25:56 +0200

    fixed switch to status 'billed'

--- sale/classes/SaleEntry.class.php
+++ sale/classes/SaleEntry.class.php
@@ -221,9 +221,8 @@ class SaleEntry extends Model {
         $self->read(['status', 'object_class', 'customer_id', 'product_id', 'price_id', 'unit_price', 'qty', 'is_billable']);
         foreach($self as $id => $entry) {
             // #memo - a sale entry can be set to 'billed' status even if not billable (to mark the entry as processed, even if there is no invoicing)
-            if( ($entry['object_class'] === 'timetrack\Project' && $entry['status'] !== 'validated')
-                    || !isset($entry['customer_id'], $entry['product_id'], $entry['price_id'], $entry['unit_price'])
-                    || $entry['qty'] <= 0 ) {
+            if( $entry['status'] !== 'validated'
+                    || !isset($entry['customer_id'], $entry['product_id'], $entry['price_id'], $entry['unit_price']) ) {
                 $result[$id] = false;
             }
         }
