7336212e287a52ceacb9756813f790d46f08d399
Cédric Françoys <cedricfrancoys@gmail.com>
2025-10-31 11:03:14 +0100

    updated

--- symbiose/classes/setting/Setting.class.php
+++ symbiose/classes/setting/Setting.class.php
@@ -8,8 +8,39 @@ namespace symbiose\setting;
 
 class Setting extends \core\setting\Setting {
 
+    public static function getColumns() {
+        return [
+
+            'setting_values_ids' => [
+                'type'              => 'one2many',
+                'foreign_object'    => 'symbiose\setting\SettingValue',
+                'foreign_field'     => 'setting_id',
+                'sort'              => 'asc',
+                'order'             => 'name',
+                'description'       => 'List of values related to the setting.'
+            ],
+
+            'setting_sequences_ids' => [
+                'type'              => 'one2many',
+                'foreign_object'    => 'symbiose\setting\SettingSequence',
+                'foreign_field'     => 'setting_id',
+                'sort'              => 'asc',
+                'order'             => 'name',
+                'description'       => 'List of sequences related to the setting.'
+            ]
+
+        ];
+    }
+
     protected static function getSelectorKeys() {
         return ['user_id', 'organisation_id'];
     }
 
+    protected static function getSettingValueClass(): string {
+        return SettingValue::class;
+    }
+
+    protected static function getSettingSequenceClass(): string {
+        return SettingSequence::class;
+    }
 }
